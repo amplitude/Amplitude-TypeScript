348e01989b5d51148a6897936fa079b845c7c5a1
Kevin Pagtakhan <kevinp@amplitude.com>
2022-03-10 09:42:24 -0800

    fix: deserialize json stored in cookies and local storage

--- packages/analytics-browser/src/storage/local-storage.ts
+++ packages/analytics-browser/src/storage/local-storage.ts
@@ -1,6 +1,6 @@
 import { Storage } from '@amplitude/analytics-types';
 
-export class LocalStorage implements Storage {
+export class LocalStorage<T> implements Storage<T> {
   isEnabled(): boolean {
     /* istanbul ignore if */
     if (typeof window === 'undefined') {
@@ -8,28 +8,37 @@ export class LocalStorage implements Storage {
     }
 
     const random = String(Date.now());
+    const testStrorage = new LocalStorage<string>();
+    const testKey = 'AMP_TEST';
     try {
-      this.set(random, random);
-      return this.get(random) === String(random);
-    } catch {
-      return false;
-    } finally {
-      this.remove(random);
-    }
-  }
-
-  get(key: string): string | null {
-    try {
-      return window.localStorage.getItem(key);
+      testStrorage.set(testKey, random);
+      const value = testStrorage.get(testKey);
+      return value === random;
     } catch {
       /* istanbul ignore next */
-      return null;
+      return false;
+    } finally {
+      testStrorage.remove(testKey);
     }
   }
 
-  set(key: string, value: string) {
+  get(key: string): T | undefined {
     try {
-      window.localStorage.setItem(key, value);
+      const value = window.localStorage.getItem(key);
+      if (!value) {
+        return undefined;
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return JSON.parse(value);
+    } catch {
+      /* istanbul ignore next */
+      return undefined;
+    }
+  }
+
+  set(key: string, value: T) {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
     } catch {
       //
     }
