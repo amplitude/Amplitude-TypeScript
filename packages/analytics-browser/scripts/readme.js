240e8e228999d18be205c0afff856fbe2dea73ea
Dean Shi <dean.xiaoshi@gmail.com>
2022-04-08 12:51:48 -0700

    fix: fix typo

--- packages/analytics-browser/scripts/readme.js
+++ packages/analytics-browser/scripts/readme.js
@@ -5,12 +5,12 @@ const cwd = process.cwd();
 const instructionsFileName = path.join(cwd, 'lib/scripts/amplitude-snippet-instructions.js');
 const instructionsContent = fs.readFileSync(instructionsFileName, 'utf-8');
 
-const readmeFilename = path.join(cwd, 'README.md');
-const readmeContent = fs.readFileSync(readmeFilename, 'utf-8');
+const readmeFileName = path.join(cwd, 'README.md');
+const readmeContent = fs.readFileSync(readmeFileName, 'utf-8');
 
 const regexpPattern = /(<!-- README_SNIPPET_BLOCK -->)((?:.|\n)*)(<!-- \/ OF README_SNIPPET_BLOCK -->)/m;
 const newReadmeContent = readmeContent.replace(regexpPattern, '$1\n```html\n' + instructionsContent + '```\n$3');
 
-fs.writeFileSync(readmeFilename, newReadmeContent);
+fs.writeFileSync(readmeFileName, newReadmeContent);
 
 console.log('Updated README.md');
