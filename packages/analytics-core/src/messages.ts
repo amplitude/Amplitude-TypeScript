210d04096f2ad5d5e1e013a2d67279efcabd46bf
Kevin Pagtakhan <kevinp@amplitude.com>
2022-04-08 22:58:27 -0700

    build: setup integration testing

--- packages/analytics-core/src/messages.ts
+++ packages/analytics-core/src/messages.ts
@@ -2,4 +2,4 @@ export const SUCCESS_MESSAGE = 'Event tracked successfully';
 export const UNEXPECTED_ERROR_MESSAGE = 'Unexpected error occurred';
 export const MAX_RETRIES_EXCEEDED_MESSAGE = 'Event rejected due to exceeded retry count';
 export const OPT_OUT_MESSAGE = 'Event skipped due to optOut config';
-export const MISSING_API_KEY_MESSAGE = 'Event skipped due to optOut config';
+export const MISSING_API_KEY_MESSAGE = 'Event rejected due to missing API key';
