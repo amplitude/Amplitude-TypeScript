23d36f8aade258b995132dafd725ada00e400916
Kevin Pagtakhan <kevinpagtakhan@users.noreply.github.com>
2022-06-29 09:40:27 -0700

    fix: remove Awaited type to support older versions of typescript (#121)

--- packages/analytics-core/src/utils/return-wrapper.ts
+++ packages/analytics-core/src/utils/return-wrapper.ts
@@ -2,6 +2,6 @@ import { AmplitudeReturn } from '@amplitude/analytics-types';
 
 export const returnWrapper =
   <T extends (...args: any) => any>(fn: T) =>
-  (...args: Parameters<T>): AmplitudeReturn<Awaited<ReturnType<T>>> => ({
+  (...args: Parameters<T>): AmplitudeReturn<ReturnType<T>> => ({
     promise: fn(...args) as ReturnType<T>,
   });
