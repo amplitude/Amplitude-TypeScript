ef46ed25fff46a78ab8da3e156808fc49418710b
Brian Giori <brian.giori@amplitude.com>
2022-07-12 16:56:02 -0700

    feat: add react-native app

---
+++ examples/react-native/react_native_app/App.tsx
@@ -0,0 +1,26 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {useEffect} from 'react';
+import {init, track} from '@amplitude/analytics-react-native';
+
+export default function App() {
+  useEffect(() => {
+    (async () => {
+      await init('a6dd847b9d2f03c816d4f3f8458cdc1d', 'briang777').promise;
+      await track('test').promise;
+    })();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
