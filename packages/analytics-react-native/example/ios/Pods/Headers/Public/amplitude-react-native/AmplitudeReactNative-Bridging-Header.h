0184ab511caf7d56d0cf4035bcbafd5dd35a8e32
Brian Giori <brian.giori@amplitude.com>
2022-07-12 17:26:45 -0700

    feat: remove example from rn

--- packages/analytics-react-native/example/ios/Pods/Headers/Public/amplitude-react-native/AmplitudeReactNative-Bridging-Header.h
+++
@@ -1,1 +0,0 @@
-../../../../../../ios/AmplitudeReactNative-Bridging-Header.h
--- packages/analytics-react-native/example/ios/Pods/Target Support Files/amplitude-react-native/amplitude-react-native-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_amplitude_react_native : NSObject
-@end
-@implementation PodsDummy_amplitude_react_native
-@end
