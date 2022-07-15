496e51e3db823938f2c7a22eb516bb698f223461
Brian Giori <brian.giori@amplitude.com>
2022-07-15 09:36:40 -0700

    chore: wip

--- examples/react-native/app/App.tsx
+++ examples/react-native/app/App.tsx
@@ -1,12 +1,31 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {useEffect} from 'react';
-import {init, track} from '@amplitude/analytics-react-native';
+import {
+  Identify,
+  identify,
+  init,
+  track,
+} from '@amplitude/analytics-react-native';
+import {Experiment} from '@amplitude/experiment-react-native-client';
+import {LogLevel} from '@amplitude/analytics-types';
 
 export default function App() {
   useEffect(() => {
     (async () => {
-      await init('a6dd847b9d2f03c816d4f3f8458cdc1d', 'briang777').promise;
-      await track('test').promise;
+      await init('a6dd847b9d2f03c816d4f3f8458cdc1d', 'briang1000', {
+        logLevel: LogLevel.Verbose,
+      }).promise;
+      const experiment = Experiment.initializeWithAmplitudeAnalytics(
+        'client-IAxMYws9vVQESrrK88aTcToyqMxiiJoR',
+        {
+          debug: true,
+        },
+      );
+      await experiment.fetch();
+      experiment.variant('react-native');
+      track('test');
+      await identify(new Identify().set('react-native-test', 'yes')).promise;
+      await experiment.fetch();
     })();
   }, []);
   return (
