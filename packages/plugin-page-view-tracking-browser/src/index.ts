aa7b05cb192e23924081a363f3567573f76a3b62
Kevin Pagtakhan <kevinp@amplitude.com>
2022-09-26 20:45:03 -0700

    fix: js script export name for marketing analytics plugins

--- packages/plugin-page-view-tracking-browser/src/index.ts
+++ packages/plugin-page-view-tracking-browser/src/index.ts
@@ -1,2 +1,3 @@
 export { pageViewTrackingPlugin } from './page-view-tracking';
+export { pageViewTrackingPlugin as plugin } from './page-view-tracking';
 export * as Types from './typings/page-view-tracking';
