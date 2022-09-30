0568941350978ad41015428ad7c58bcf5cefd160
Kevin Pagtakhan <kevinp@amplitude.com>
2022-09-30 09:02:55 -0700

    refactor: convert marketing analytics browser context plugin to fn definition

--- packages/marketing-analytics-browser/src/plugins/context.ts
+++ packages/marketing-analytics-browser/src/plugins/context.ts
@@ -1,21 +1,15 @@
 import { Event, PluginType, EnrichmentPlugin } from '@amplitude/analytics-types';
 import { VERSION } from '../version';
 
-export class Context implements EnrichmentPlugin {
-  name = 'context';
-  type = PluginType.ENRICHMENT as const;
-
-  library = `amplitude-ma-ts/${VERSION}`;
-
-  setup(): Promise<undefined> {
-    return Promise.resolve(undefined);
-  }
-
-  async execute(context: Event): Promise<Event> {
-    const event: Event = {
+export const context = (): EnrichmentPlugin => {
+  const library = `amplitude-ma-ts/${VERSION}`;
+  return {
+    name: 'context',
+    type: PluginType.ENRICHMENT,
+    setup: async () => undefined,
+    execute: async (context: Event) => ({
       ...context,
-      library: this.library,
-    };
-    return event;
-  }
-}
+      library,
+    }),
+  };
+};
