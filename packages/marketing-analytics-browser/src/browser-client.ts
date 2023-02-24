9e88bf0f182fbf0f998dcc0cfa5c8b7408c685f5
Kevin Pagtakhan <kevinp@amplitude.com>
2023-02-23 23:47:46 -0800

    fix: prevent usage of defaultTracking for marketing analytics

--- packages/marketing-analytics-browser/src/browser-client.ts
+++ packages/marketing-analytics-browser/src/browser-client.ts
@@ -24,6 +24,7 @@ export const createInstance = (): Client => {
 
     await client.add(context()).promise;
 
+    delete browserOptions.defaultTracking;
     if (pageViewTracking === true) {
       browserOptions.defaultTracking = {
         pageViews: {
