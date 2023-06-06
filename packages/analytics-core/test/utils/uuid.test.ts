171cc06119f364de30017b033621cda5f83fcdcb
Xinyi Ye <xinyi.ye@amplitude.com>
2023-06-06 15:20:50 -0700

    fix: uuid collision

--- packages/analytics-core/test/utils/uuid.test.ts
+++ packages/analytics-core/test/utils/uuid.test.ts
@@ -6,4 +6,13 @@ describe('UUID', () => {
     expect(uuid.length).toEqual(36);
     expect(uuid.substring(14, 15)).toEqual('4');
   });
+
+  test('should generate a unique UUID-4', () => {
+    const ids = new Set();
+    const count = 10000;
+    for (let i = 0; i < count; i++) {
+      ids.add(UUID());
+    }
+    expect(ids.size).toEqual(count);
+  });
 });
