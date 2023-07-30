deb93a48412467971460a60653c1f4b1c7a5417e
Andrey Sokolov <andrey.sokolov@amplitude.com>
2023-07-30 11:12:14 +0400

    fix: catch/log native Android exceptions

--- packages/analytics-react-native/android/src/main/java/com/amplitude/reactnative/AmplitudeReactNativeModule.kt
+++ packages/analytics-react-native/android/src/main/java/com/amplitude/reactnative/AmplitudeReactNativeModule.kt
@@ -53,53 +53,94 @@ class AmplitudeReactNativeModule(private val reactContext: ReactApplicationConte
 
     @ReactMethod
     private fun getLegacySessionData(instanceName: String?, promise: Promise) {
-        val storage = LegacyDatabaseStorageProvider.getStorage(reactContext.applicationContext, instanceName)
-        val deviceId = storage.getValue(DEVICE_ID_KEY)
-        val userId = storage.getValue(USER_ID_KEY)
-        val previousSessionId = storage.getLongValue(PREVIOUS_SESSION_ID_KEY)
-        val lastEventTime = storage.getLongValue(LAST_EVENT_TIME_KEY)
-        val lastEventId = storage.getLongValue(LAST_EVENT_ID_KEY)
-        promise.resolve(WritableNativeMap().apply {
-            if (deviceId != null) {
-                putString("deviceId", deviceId)
-            }
-            if (userId != null) {
-                putString("userId", userId)
-            }
-            if (previousSessionId != null) {
-                putDouble("sessionId", previousSessionId.toDouble())
-            }
-            if (lastEventTime != null) {
-                putDouble("lastEventTime", lastEventTime.toDouble())
-            }
-            if (lastEventId != null) {
-                putDouble("lastEventId", lastEventId.toDouble())
-            }
-        })
+        try {
+            val storage = LegacyDatabaseStorageProvider.getStorage(reactContext.applicationContext, instanceName)
+            val deviceId = getLegacyValue(storage, DEVICE_ID_KEY)
+            val userId = getLegacyValue(storage, USER_ID_KEY)
+            val previousSessionId = getLegacyLongValue(storage, PREVIOUS_SESSION_ID_KEY)
+            val lastEventTime = getLegacyLongValue(storage, LAST_EVENT_TIME_KEY)
+            val lastEventId = getLegacyLongValue(storage, LAST_EVENT_ID_KEY)
+            promise.resolve(WritableNativeMap().apply {
+                if (deviceId != null) {
+                    putString("deviceId", deviceId)
+                }
+                if (userId != null) {
+                    putString("userId", userId)
+                }
+                if (previousSessionId != null) {
+                    putDouble("sessionId", previousSessionId.toDouble())
+                }
+                if (lastEventTime != null) {
+                    putDouble("lastEventTime", lastEventTime.toDouble())
+                }
+                if (lastEventId != null) {
+                    putDouble("lastEventId", lastEventId.toDouble())
+                }
+            })
+        } catch (e: Exception) {
+            LogcatLogger.logger.error(
+                "can't get legacy session data: $e"
+            )
+        }
+    }
+
+    private fun getLegacyValue(storage: LegacyDatabaseStorage, key: String): String? {
+        try {
+            return storage.getValue(key)
+        } catch (e: Exception) {
+            LogcatLogger.logger.error(
+                "can't get legacy $key: $e"
+            )
+            return null
+        }
+    }
+
+    private fun getLegacyLongValue(storage: LegacyDatabaseStorage, key: String): Long? {
+        try {
+            return storage.getLongValue(key)
+        } catch (e: Exception) {
+            LogcatLogger.logger.error(
+                "can't get legacy $key: $e"
+            )
+            return null
+        }
     }
 
     @ReactMethod
     private fun getLegacyEvents(instanceName: String?, eventKind: String, promise: Promise) {
-        val storage = LegacyDatabaseStorageProvider.getStorage(reactContext.applicationContext, instanceName)
-        val jsonEvents = when (eventKind) {
-            "event" -> storage.readEvents()
-            "identify" -> storage.readIdentifies()
-            "interceptedIdentify" -> storage.readInterceptedIdentifies()
-            else -> listOf()
-        }
+        try {
+            val storage = LegacyDatabaseStorageProvider.getStorage(reactContext.applicationContext, instanceName)
+            val jsonEvents = when (eventKind) {
+                "event" -> storage.readEvents()
+                "identify" -> storage.readIdentifies()
+                "interceptedIdentify" -> storage.readInterceptedIdentifies()
+                else -> listOf()
+            }
 
-        val events = WritableNativeArray()
-        jsonEvents.forEach { event -> events.pushString(event.toString()) }
-        promise.resolve(events)
+            val events = WritableNativeArray()
+            jsonEvents.forEach { event -> events.pushString(event.toString()) }
+            promise.resolve(events)
+        } catch (e: Exception) {
+            LogcatLogger.logger.error(
+                "can't get legacy ${eventKind}s: $e"
+            )
+            promise.resolve(WritableNativeArray())
+        }
     }
 
     @ReactMethod
     private fun removeLegacyEvent(instanceName: String?, eventKind: String, eventId: Int) {
-        val storage = LegacyDatabaseStorageProvider.getStorage(reactContext.applicationContext, instanceName)
-        when (eventKind) {
-            "event" -> storage.removeEvent(eventId)
-            "identify" -> storage.removeIdentify(eventId)
-            "interceptedIdentify" -> storage.removeInterceptedIdentify(eventId)
+        try {
+            val storage = LegacyDatabaseStorageProvider.getStorage(reactContext.applicationContext, instanceName)
+            when (eventKind) {
+                "event" -> storage.removeEvent(eventId)
+                "identify" -> storage.removeIdentify(eventId)
+                "interceptedIdentify" -> storage.removeInterceptedIdentify(eventId)
+            }
+        } catch (e: Exception) {
+            LogcatLogger.logger.error(
+                "can't remove legacy $eventKind with id=$eventId: $e"
+            )
         }
     }
 }
