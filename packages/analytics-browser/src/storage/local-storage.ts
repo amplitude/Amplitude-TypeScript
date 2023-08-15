232a607108baba4427723352212c650e09ee01a6
Xinyi Ye <xinyi.ye@amplitude.com>
2023-08-15 09:25:21 -0700

    feat: limit LocalStorage (#528)

--- packages/analytics-browser/src/storage/local-storage.ts
+++ packages/analytics-browser/src/storage/local-storage.ts
@@ -1,8 +1,29 @@
 import { getGlobalScope } from '@amplitude/analytics-client-common';
 import { BrowserStorage } from './browser-storage';
+import { Logger } from '@amplitude/analytics-types';
 
+const MAX_ARRAY_LENGTH = 1000;
+
+interface LocalStorageOptions {
+  loggerProvider?: Logger;
+}
 export class LocalStorage<T> extends BrowserStorage<T> {
-  constructor() {
+  loggerProvider?: Logger;
+
+  constructor(config?: LocalStorageOptions) {
     super(getGlobalScope()?.localStorage);
+    this.loggerProvider = config?.loggerProvider;
+  }
+
+  async set(key: string, value: T): Promise<void> {
+    if (Array.isArray(value) && value.length > MAX_ARRAY_LENGTH) {
+      const droppedEventsCount = value.length - MAX_ARRAY_LENGTH;
+      await super.set(key, value.slice(0, MAX_ARRAY_LENGTH) as T);
+      this.loggerProvider?.error(
+        `Failed to save ${droppedEventsCount} events because the queue length exceeded ${MAX_ARRAY_LENGTH}.`,
+      );
+    } else {
+      await super.set(key, value);
+    }
   }
 }
