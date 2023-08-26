cd04c90184de768560a163e485404b9c5c684b43
Xinyi Ye <xinyi.ye@amplitude.com>
2023-08-25 17:05:39 -0700

    chore: delete comments

--- packages/analytics-browser/src/plugins/destination.ts
+++ packages/analytics-browser/src/plugins/destination.ts
@@ -2,9 +2,6 @@ import { Destination as CoreDestination, buildResult } from '@amplitude/analytic
 import { BrowserConfig, DestinationContext as Context } from '@amplitude/analytics-types';
 
 export class Destination extends CoreDestination {
-  // this.config is defined in setup() which will always be called first
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   config: BrowserConfig;
 
   constructor(config: BrowserConfig) {
