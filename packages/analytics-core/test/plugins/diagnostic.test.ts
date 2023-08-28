4521a9acd2164b692c34a0e4a03ad8c930a11cb1
Xinyi Ye <xinyi.ye@amplitude.com>
2023-08-28 00:26:50 -0700

    test: diagnostic

---
+++ packages/analytics-core/test/plugins/diagnostic.test.ts
@@ -0,0 +1,60 @@
+import { Diagnostic } from '../../src/plugins/diagnostic';
+
+jest.mock('node-fetch'); // Mocking node-fetch library
+jest.useFakeTimers();
+
+describe('Diagnostic', () => {
+  let diagnostic: Diagnostic;
+  const eventCount = 5;
+  const code = 200;
+
+  beforeEach(() => {
+    diagnostic = new Diagnostic();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks(); // Restore mocked functions after each test
+  });
+
+  test('should add events to the queue when track method is called', async () => {
+    await diagnostic.track(eventCount, code, 'Test message');
+
+    expect(diagnostic.queue).toHaveLength(1);
+    expect(diagnostic.queue[0].event_properties.event_count).toBe(eventCount);
+    expect(diagnostic.queue[0].event_properties.response_error_code).toBe(code);
+    expect(diagnostic.queue[0].event_properties.trigger).toBe('Test message');
+    expect(diagnostic.queue[0].library).toBe('diagnostic-test-library');
+  });
+
+  test('should schedule flush when track is called for the first time 0', async () => {
+    const setTimeoutMock = jest.spyOn(global, 'setTimeout');
+
+    await diagnostic.track(eventCount, code, 'Test message');
+
+    jest.advanceTimersByTime(diagnostic.delay);
+    expect(setTimeoutMock).toHaveBeenCalledTimes(1);
+    expect(setTimeoutMock.mock.calls[0][0]).toBeInstanceOf(Function);
+    expect(setTimeoutMock.mock.calls[0][1]).toBe(diagnostic.delay);
+    setTimeoutMock.mockRestore();
+  });
+
+  test('should clear scheduled timeout when flush is called', async () => {
+    const clearTimeoutMock = jest.spyOn(global, 'clearTimeout');
+    const setTimeoutMock = jest.spyOn(global, 'setTimeout');
+
+    await diagnostic.track(eventCount, code, 'Scheduled timeout test');
+    await diagnostic.flush();
+
+    expect(setTimeoutMock).toHaveBeenCalledTimes(1);
+    expect(clearTimeoutMock).toHaveBeenCalledTimes(1);
+    expect(diagnostic.scheduled).toBeNull();
+  });
+
+  test('should not be called, use track() instead', async () => {
+    const context = { event_type: 'custom event' };
+
+    const result = await diagnostic.execute(context);
+
+    expect(result.message).toBe('this method should not be called, use track() instead');
+  });
+});
