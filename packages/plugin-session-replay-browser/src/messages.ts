41faedceaba39ea743a83d734fc0462b2fb072df
Kelly Wallach <kelly.wallach@amplitude.com>
2023-08-31 13:28:18 -0400

    fix(session replay plugin): delete unused file

--- packages/plugin-session-replay-browser/src/messages.ts
+++
@@ -1,5 +0,0 @@
-export const getSuccessMessage = (sessionId: number) =>
-  `Session replay event batch tracked successfully for session id ${sessionId}`;
-export const UNEXPECTED_ERROR_MESSAGE = 'Unexpected error occurred';
-export const MAX_RETRIES_EXCEEDED_MESSAGE = 'Session replay event batch rejected due to exceeded retry count';
-export const STORAGE_FAILURE = 'Failed to store session replay events in IndexedDB';
