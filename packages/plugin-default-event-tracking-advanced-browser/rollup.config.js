c20e9e626bf3f6163d445d95d997fd0113a5e95b
Marvin Liu <marvin@amplitude.com>
2023-10-11 09:14:53 -0700

    refactor: update output function name

--- packages/plugin-default-event-tracking-advanced-browser/rollup.config.js
+++ packages/plugin-default-event-tracking-advanced-browser/rollup.config.js
@@ -1,6 +1,6 @@
 import { iife, umd } from '../../scripts/build/rollup.config';
 
 iife.input = umd.input;
-iife.output.name = 'amplitudeAutoTrackingPlugin';
+iife.output.name = 'amplitudeDefaultEventTrackingAdvancedPlugin';
 
 export default [umd, iife];
