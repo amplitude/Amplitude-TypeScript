f6e7ef7bfc6f1ac6697cc3f005fb702636df2d55
Marvin Liu <marvin@amplitude.com>
2024-01-31 09:28:50 -0800

    feat: bump selector version to 0.1.1 (#656)

--- packages/plugin-default-event-tracking-advanced-browser/src/constants.ts
+++ packages/plugin-default-event-tracking-advanced-browser/src/constants.ts
@@ -22,7 +22,7 @@ export const AMPLITUDE_EVENT_PROP_VIEWPORT_WIDTH = '[Amplitude] Viewport Width';
 // Visual Tagging related constants
 export const AMPLITUDE_ORIGIN = 'https://app.amplitude.com';
 export const AMPLITUDE_VISUAL_TAGGING_SELECTOR_SCRIPT_URL =
-  'https://cdn.amplitude.com/libs/visual-tagging-selector-0.1.0.js.gz';
+  'https://cdn.amplitude.com/libs/visual-tagging-selector-0.1.1.js.gz';
 // This is the class name used by the visual tagging selector to highlight the selected element.
 // Should not use this class in the selector.
 export const AMPLITUDE_VISUAL_TAGGING_HIGHLIGHT_CLASS = 'amp-visual-tagging-selector-highlight';
