ccd3766d2b2433147a8051e7fdce17b477bd2a2d
izaaz.yunus <izaaz@amplitude.com>
2024-03-26 14:54:53 -0700

    fix: register download and form listeners after window is loaded

--- packages/analytics-browser/src/plugins/file-download-tracking.ts
+++ packages/analytics-browser/src/plugins/file-download-tracking.ts
@@ -30,75 +30,81 @@ export const fileDownloadTracking = (): EnrichmentPlugin => {
   const name = '@amplitude/plugin-file-download-tracking-browser';
   const type = 'enrichment';
   const setup = async (config: BrowserConfig, amplitude: BrowserClient) => {
-    /* istanbul ignore if */
-    if (!amplitude) {
-      // TODO: Add required minimum version of @amplitude/analytics-browser
-      config.loggerProvider.warn(
-        'File download tracking requires a later version of @amplitude/analytics-browser. File download events are not tracked.',
-      );
-      return;
-    }
-
-    /* istanbul ignore if */
-    if (typeof document === 'undefined') {
-      return;
-    }
-
-    const addFileDownloadListener = (a: HTMLAnchorElement) => {
-      let url: URL;
-      try {
-        // eslint-disable-next-line no-restricted-globals
-        url = new URL(a.href, window.location.href);
-      } catch {
-        /* istanbul ignore next */
+    // The form interaction plugin observes changes in the dom. For this to work correctly, the observer can only be setup
+    // after the body is built. When Amplitud gets initialized in a script tag, the body tag is still unavailable. So register this
+    // only after the window is loaded
+    // eslint-disable-next-line no-restricted-globals
+    window.addEventListener('load', function () {
+      /* istanbul ignore if */
+      if (!amplitude) {
+        // TODO: Add required minimum version of @amplitude/analytics-browser
+        config.loggerProvider.warn(
+          'File download tracking requires a later version of @amplitude/analytics-browser. File download events are not tracked.',
+        );
         return;
       }
-      const result = ext.exec(url.href);
-      const fileExtension = result?.[1];
 
-      if (fileExtension) {
-        addEventListener(a, 'click', () => {
-          if (fileExtension) {
-            amplitude.track(DEFAULT_FILE_DOWNLOAD_EVENT, {
-              [FILE_EXTENSION]: fileExtension,
-              [FILE_NAME]: url.pathname,
-              [LINK_ID]: a.id,
-              [LINK_TEXT]: a.text,
-              [LINK_URL]: a.href,
-            });
-          }
-        });
+      /* istanbul ignore if */
+      if (typeof document === 'undefined') {
+        return;
       }
-    };
 
-    const ext =
-      /\.(pdf|xlsx?|docx?|txt|rtf|csv|exe|key|pp(s|t|tx)|7z|pkg|rar|gz|zip|avi|mov|mp4|mpe?g|wmv|midi?|mp3|wav|wma)$/;
+      const addFileDownloadListener = (a: HTMLAnchorElement) => {
+        let url: URL;
+        try {
+          // eslint-disable-next-line no-restricted-globals
+          url = new URL(a.href, window.location.href);
+        } catch {
+          /* istanbul ignore next */
+          return;
+        }
+        const result = ext.exec(url.href);
+        const fileExtension = result?.[1];
 
-    // Adds listener to existing anchor tags
-    const links = Array.from(document.getElementsByTagName('a'));
-    links.forEach(addFileDownloadListener);
-
-    // Adds listener to anchor tags added after initial load
-    /* istanbul ignore else */
-    if (typeof MutationObserver !== 'undefined') {
-      observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          mutation.addedNodes.forEach((node) => {
-            if (node.nodeName === 'A') {
-              addFileDownloadListener(node as HTMLAnchorElement);
-            }
-            if ('querySelectorAll' in node && typeof node.querySelectorAll === 'function') {
-              Array.from(node.querySelectorAll('a') as HTMLAnchorElement[]).map(addFileDownloadListener);
+        if (fileExtension) {
+          addEventListener(a, 'click', () => {
+            if (fileExtension) {
+              amplitude.track(DEFAULT_FILE_DOWNLOAD_EVENT, {
+                [FILE_EXTENSION]: fileExtension,
+                [FILE_NAME]: url.pathname,
+                [LINK_ID]: a.id,
+                [LINK_TEXT]: a.text,
+                [LINK_URL]: a.href,
+              });
             }
           });
-        });
-      });
+        }
+      };
 
-      observer.observe(document.body, {
-        subtree: true,
-        childList: true,
-      });
-    }
+      const ext =
+        /\.(pdf|xlsx?|docx?|txt|rtf|csv|exe|key|pp(s|t|tx)|7z|pkg|rar|gz|zip|avi|mov|mp4|mpe?g|wmv|midi?|mp3|wav|wma)$/;
+
+      // Adds listener to existing anchor tags
+      const links = Array.from(document.getElementsByTagName('a'));
+      links.forEach(addFileDownloadListener);
+
+      // Adds listener to anchor tags added after initial load
+      /* istanbul ignore else */
+      if (typeof MutationObserver !== 'undefined') {
+        observer = new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+            mutation.addedNodes.forEach((node) => {
+              if (node.nodeName === 'A') {
+                addFileDownloadListener(node as HTMLAnchorElement);
+              }
+              if ('querySelectorAll' in node && typeof node.querySelectorAll === 'function') {
+                Array.from(node.querySelectorAll('a') as HTMLAnchorElement[]).map(addFileDownloadListener);
+              }
+            });
+          });
+        });
+
+        observer.observe(document.body, {
+          subtree: true,
+          childList: true,
+        });
+      }
+    });
   };
   const execute = async (event: Event) => event;
   const teardown = async () => {
