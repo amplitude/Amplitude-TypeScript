6bbb69d971dfabab533c60930551bb5e3578bed0
Marvin Liu <marvin@amplitude.com>
2024-04-23 19:10:17 -0700

    feat: update bookmarklet to use autocapture plugin (#728)

--- scripts/version/create-bookmarklet-snippet.js
+++ scripts/version/create-bookmarklet-snippet.js
@@ -37,7 +37,7 @@ const integrity = algorithm + '-' + crypto.createHash(algorithm).update(inputTex
 const version = getVersion() || '';
 const ingestionSourceName = 'browser-typescript-bookmarklet';
 const ingestionSourceVersion = '1.0.0';
-const autoTrackingPluginVersion = '0.7.2';
+const autoTrackingPluginVersion = '0.9.0';
 const outputText =
   header +
   snippet(
