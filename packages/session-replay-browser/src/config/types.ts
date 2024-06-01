d86fb5b74b89f6a703153db31da4b06a21bd1ae4
Lew Gordon <lew.gordon@amplitude.com>
2024-05-31 23:24:15 -0400

    style(session-replay-browser): run prettier

--- packages/session-replay-browser/src/config/types.ts
+++ packages/session-replay-browser/src/config/types.ts
@@ -17,9 +17,9 @@ export interface SessionReplayRemoteConfigAPIResponse {
 }
 
 export type MaskLevel =
-  'light' | // only mask a subset of inputs that’s deemed sensitive - password, credit card, telephone #, email. These are information we never want to capture.
-  'medium' | // mask all inputs
-  'conservative' // mask all inputs and all texts
+  | 'light' // only mask a subset of inputs that’s deemed sensitive - password, credit card, telephone #, email. These are information we never want to capture.
+  | 'medium' // mask all inputs
+  | 'conservative'; // mask all inputs and all texts
 
 export const DEFAULT_MASK_LEVEL = 'medium';
 
