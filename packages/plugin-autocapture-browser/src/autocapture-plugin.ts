a2558c83d3373aabeaf26b9196883f52028b5f67
Xinyi Ye <xinyi.ye@amplitude.com>
2024-06-07 10:46:06 -0700

    fix(plugin-autocapture-browser): export AutocaptureOptions (#766)

--- packages/plugin-autocapture-browser/src/autocapture-plugin.ts
+++ packages/plugin-autocapture-browser/src/autocapture-plugin.ts
@@ -34,7 +34,7 @@ interface EventListener {
   handler: (event: Event) => void;
 }
 
-interface Options {
+export interface AutocaptureOptions {
   /**
    * List of CSS selectors to allow auto tracking on.
    * When provided, allow elements matching any selector to be tracked.
@@ -74,7 +74,7 @@ interface Options {
   };
 }
 
-export const autocapturePlugin = (options: Options = {}): BrowserEnrichmentPlugin => {
+export const autocapturePlugin = (options: AutocaptureOptions = {}): BrowserEnrichmentPlugin => {
   const {
     cssSelectorAllowlist = DEFAULT_CSS_SELECTOR_ALLOWLIST,
     pageUrlAllowlist,
--- packages/plugin-autocapture-browser/src/index.ts
+++ packages/plugin-autocapture-browser/src/index.ts
@@ -3,5 +3,6 @@ export {
   autocapturePlugin,
   DEFAULT_CSS_SELECTOR_ALLOWLIST,
   DEFAULT_DATA_ATTRIBUTE_PREFIX,
+  AutocaptureOptions,
 } from './autocapture-plugin';
 export { Messenger, Action, ActionData, Message, WindowMessenger } from './libs/messenger';
