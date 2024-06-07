583dbb5d2559c9aaa67a39cf19eb49028f79d517
Xinyi Ye <xinyi.ye@amplitude.com>
2024-06-06 17:27:12 -0700

    chore(plugin-autocapture-browser): export config options

--- packages/plugin-autocapture-browser/src/index.ts
+++ packages/plugin-autocapture-browser/src/index.ts
@@ -3,5 +3,6 @@ export {
   autocapturePlugin,
   DEFAULT_CSS_SELECTOR_ALLOWLIST,
   DEFAULT_DATA_ATTRIBUTE_PREFIX,
+  Options as AutocaptureOptions,
 } from './autocapture-plugin';
 export { Messenger, Action, ActionData, Message, WindowMessenger } from './libs/messenger';
