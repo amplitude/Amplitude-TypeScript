8705cf78f9bc9a215b339adffd90608cc62b63b8
Lew Gordon <lew.gordon@amplitude.com>
2024-06-14 16:14:07 -0400

    test(session-replay-browser): fix package-session-replay-browser jest config

--- packages/plugin-session-replay-browser/jest.config.js
+++ packages/plugin-session-replay-browser/jest.config.js
@@ -3,6 +3,11 @@ const package = require('./package');
 
 module.exports = {
   ...baseConfig,
+  transform: {
+    ...baseConfig.transform,
+    '\\.[jt]sx?$': ['babel-jest', { plugins: ['@babel/plugin-transform-modules-commonjs'] }], // needed for @medv/finder
+  },
+  transformIgnorePatterns: [`../../node_modules/(?!@medv)`],
   displayName: package.name,
   rootDir: '.',
   testEnvironment: 'jsdom',
