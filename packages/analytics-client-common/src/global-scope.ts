4176f5f0571dbd535761f92c997fb825253807a5
Zac Bennett <zac.bennett@CRFWK72K0F.local>
2024-06-24 09:48:35 -0700

    fix: checking for undefined and added tests

--- packages/analytics-client-common/src/global-scope.ts
+++ packages/analytics-client-common/src/global-scope.ts
@@ -5,7 +5,7 @@ export const getGlobalScope = (): typeof globalThis | undefined => {
   // This should only be used for integrations with Amplitude that are not running in a browser environment
   //   We need to specify the name of the global variable as a string to prevent it from being minified
   const ampIntegrationContextName = 'ampIntegrationContext' as keyof typeof globalThis;
-  if (typeof globalThis[ampIntegrationContextName] !== 'undefined') {
+  if (typeof globalThis !== 'undefined' && typeof globalThis[ampIntegrationContextName] !== 'undefined') {
     return globalThis[ampIntegrationContextName] as typeof globalThis;
   }
   if (typeof globalThis !== 'undefined') {
