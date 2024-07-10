d91e43775503133ea055fd663a49d9a23a325a7c
Alvin Wang <alvin.wang@amplitude.com>
2024-07-09 18:59:04 -0700

    fix: reverse hierarchy and add tests

--- packages/plugin-autocapture-browser/test/hierarchy.test.ts
+++ packages/plugin-autocapture-browser/test/hierarchy.test.ts
@@ -1,4 +1,4 @@
-import { getAncestors, getElementProperties, getHierarchy } from '../src/hierarchy';
+import * as HierarchyUtil from '../src/hierarchy';
 
 describe('autocapture-plugin hierarchy', () => {
   afterEach(() => {
@@ -19,7 +19,7 @@ describe('autocapture-plugin hierarchy', () => {
       `;
 
       const nullElement = document.getElementById('null-element');
-      expect(getElementProperties(nullElement)).toEqual(null);
+      expect(HierarchyUtil.getElementProperties(nullElement)).toEqual(null);
     });
 
     test('should return tag and index information if element has siblings', () => {
@@ -41,7 +41,7 @@ describe('autocapture-plugin hierarchy', () => {
       `;
 
       const inner4 = document.getElementById('inner4');
-      expect(getElementProperties(inner4)).toEqual({
+      expect(HierarchyUtil.getElementProperties(inner4)).toEqual({
         id: 'inner4',
         index: 3,
         indexOfType: 1,
@@ -63,7 +63,7 @@ describe('autocapture-plugin hierarchy', () => {
       `;
 
       const inner = document.getElementById('inner');
-      expect(getElementProperties(inner)).toEqual({
+      expect(HierarchyUtil.getElementProperties(inner)).toEqual({
         id: 'inner',
         index: 0,
         indexOfType: 0,
@@ -81,7 +81,7 @@ describe('autocapture-plugin hierarchy', () => {
       `;
 
       const inner = document.getElementById('inner');
-      expect(getElementProperties(inner)).toEqual({
+      expect(HierarchyUtil.getElementProperties(inner)).toEqual({
         id: 'inner',
         index: 0,
         indexOfType: 0,
@@ -94,7 +94,7 @@ describe('autocapture-plugin hierarchy', () => {
   test('should not fail when parent element is null', () => {
     const parentlessElement = document.createElement('div');
 
-    expect(getElementProperties(parentlessElement)).toEqual({
+    expect(HierarchyUtil.getElementProperties(parentlessElement)).toEqual({
       tag: 'div',
     });
   });
@@ -106,7 +106,7 @@ describe('autocapture-plugin hierarchy', () => {
     `;
 
       const target = document.getElementById('target');
-      expect(getElementProperties(target)).toEqual({
+      expect(HierarchyUtil.getElementProperties(target)).toEqual({
         id: 'target',
         index: 0,
         indexOfType: 0,
@@ -123,7 +123,7 @@ describe('autocapture-plugin hierarchy', () => {
     `;
 
       const target = document.getElementById('target');
-      expect(getElementProperties(target)).toEqual({
+      expect(HierarchyUtil.getElementProperties(target)).toEqual({
         id: 'target',
         index: 0,
         indexOfType: 0,
@@ -140,7 +140,7 @@ describe('autocapture-plugin hierarchy', () => {
     `;
 
       const target = document.getElementById('target');
-      expect(getElementProperties(target)).toEqual({
+      expect(HierarchyUtil.getElementProperties(target)).toEqual({
         id: 'target',
         index: 0,
         indexOfType: 0,
@@ -155,7 +155,7 @@ describe('autocapture-plugin hierarchy', () => {
     `;
 
       const target = document.getElementById('target');
-      expect(getElementProperties(target)).toEqual({
+      expect(HierarchyUtil.getElementProperties(target)).toEqual({
         id: 'target',
         index: 0,
         indexOfType: 0,
@@ -179,7 +179,7 @@ describe('getAncestors', () => {
     `;
 
     const inner = document.getElementById('inner');
-    expect(getAncestors(inner)).toEqual([
+    expect(HierarchyUtil.getAncestors(inner)).toEqual([
       inner,
       document.getElementById('parent1'),
       document.getElementById('parent2'),
@@ -189,7 +189,7 @@ describe('getAncestors', () => {
 
   test('should not fail when element is null', () => {
     const nullElement = null;
-    expect(getAncestors(nullElement)).toEqual([]);
+    expect(HierarchyUtil.getAncestors(nullElement)).toEqual([]);
   });
 });
 
@@ -210,17 +210,12 @@ describe('getHierarchy', () => {
 
     const inner2 = document.getElementById('inner2');
 
-    expect(getHierarchy(inner2)).toEqual([
+    expect(HierarchyUtil.getHierarchy(inner2)).toEqual([
       {
+        id: 'inner2',
         index: 1,
-        indexOfType: 0,
-        prevSib: 'head',
-        tag: 'body',
-      },
-      {
-        id: 'parent2',
-        index: 0,
-        indexOfType: 0,
+        indexOfType: 1,
+        prevSib: 'div',
         tag: 'div',
       },
       {
@@ -230,22 +225,28 @@ describe('getHierarchy', () => {
         tag: 'div',
       },
       {
-        id: 'inner2',
-        index: 1,
-        indexOfType: 1,
-        prevSib: 'div',
+        id: 'parent2',
+        index: 0,
+        indexOfType: 0,
         tag: 'div',
+      },
+      {
+        index: 1,
+        indexOfType: 0,
+        prevSib: 'head',
+        tag: 'body',
       },
     ]);
   });
 
   test('should not fail when element is null', () => {
     const nullElement = null;
-    expect(getHierarchy(nullElement)).toEqual([]);
+    expect(HierarchyUtil.getHierarchy(nullElement)).toEqual([]);
   });
 
-  test('should cut off hierarchy output nodes to stay less than or equal to 1024 chars', () => {
-    document.getElementsByTagName('body')[0].innerHTML = `
+  describe('[Amplitude] Element Hierarchy property:', () => {
+    test('should cut off hierarchy output nodes to stay less than or equal to 1024 chars', () => {
+      document.getElementsByTagName('body')[0].innerHTML = `
       <div id="parent2">
         <div id="parent1"
           long-attribute="${'a'.repeat(2000)}end"
@@ -253,7 +254,7 @@ describe('getHierarchy', () => {
           long-attribute3="${'a'.repeat(128)}"
           long-attribute4="${'a'.repeat(128)}"
           long-attribute5="${'a'.repeat(128)}"
-          attribute6="${'a'.repeat(85)}"
+          attribute6="${'a'.repeat(8)}"
         >
           <div id="inner12345">
             xxx
@@ -262,37 +263,141 @@ describe('getHierarchy', () => {
       </div>
     `;
 
-    const inner12345 = document.getElementById('inner12345');
-    const innerHierarchy = getHierarchy(inner12345);
-    expect(innerHierarchy).toEqual([
-      {
-        id: 'parent2',
-        index: 0,
-        indexOfType: 0,
-        tag: 'div',
-      },
-      {
-        id: 'parent1',
-        index: 0,
-        indexOfType: 0,
-        tag: 'div',
-        attrs: {
-          'long-attribute': 'a'.repeat(128),
-          'long-attribute2': 'a'.repeat(128),
-          'long-attribute3': 'a'.repeat(128),
-          'long-attribute4': 'a'.repeat(128),
-          'long-attribute5': 'a'.repeat(128),
-          attribute6: 'a'.repeat(85),
+      const inner12345 = document.getElementById('inner12345');
+      const innerHierarchy = HierarchyUtil.getHierarchy(inner12345);
+      // expect innerHierarchy to not have body to stay under 1024 chars
+      expect(innerHierarchy).toEqual([
+        {
+          id: 'inner12345',
+          index: 0,
+          indexOfType: 0,
+          tag: 'div',
         },
-      },
-      {
-        id: 'inner12345',
-        index: 0,
-        indexOfType: 0,
-        tag: 'div',
-      },
-    ]);
+        {
+          id: 'parent1',
+          index: 0,
+          indexOfType: 0,
+          tag: 'div',
+          attrs: {
+            'long-attribute': 'a'.repeat(128),
+            'long-attribute2': 'a'.repeat(128),
+            'long-attribute3': 'a'.repeat(128),
+            'long-attribute4': 'a'.repeat(128),
+            'long-attribute5': 'a'.repeat(128),
+            attribute6: 'a'.repeat(8),
+          },
+        },
+        {
+          id: 'parent2',
+          index: 0,
+          indexOfType: 0,
+          tag: 'div',
+        },
+      ]);
+      const resultLength = JSON.stringify(innerHierarchy).length;
+      expect(resultLength).toBeLessThanOrEqual(1024);
+      expect(resultLength).toEqual(947);
+    });
+  });
 
-    expect(JSON.stringify(innerHierarchy).length).toEqual(1024);
+  describe('ensureUnicodePythonCompatible', () => {
+    // Test null values
+    test('handles null values', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible(null)).toBeNull();
+      expect(HierarchyUtil.ensureUnicodePythonCompatible(null, true)).toBe('None');
+    });
+
+    test('handles simple string', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible('abc')).toBe('abc');
+    });
+
+    test('handles string with double quote', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible('ab"c', true)).toBe(`'ab"c'`);
+    });
+
+    test('handles string with single quote', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible(`ab'c`, true)).toBe(`"ab\\'c"`);
+    });
+
+    test('handles number', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible(123)).toBe('123');
+    });
+
+    test('handles boolean values', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible(true)).toBe('True');
+      expect(HierarchyUtil.ensureUnicodePythonCompatible(false)).toBe('False');
+    });
+
+    test('handles array with mixed types', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible([123, 'abc'])).toBe("[123, 'abc']");
+    });
+
+    test('handles object with mixed types', () => {
+      const result = HierarchyUtil.ensureUnicodePythonCompatible({ abc: 123, def: 'what' });
+      expect(result === "{'abc': 123, 'def': 'what'}" || result === "{'def': 'what', 'abc': 123}").toBeTruthy();
+    });
+
+    test('handles object with value of string with single quote', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible({ key: `ab'c` }, true)).toBe(`{\\'key\\': "ab\\d\\'c"}`);
+    });
+
+    // Test edge cases
+    test('handles edge cases', () => {
+      expect(HierarchyUtil.ensureUnicodePythonCompatible('')).toBe('');
+      expect(HierarchyUtil.ensureUnicodePythonCompatible('', true)).toBe("''");
+      expect(HierarchyUtil.ensureUnicodePythonCompatible({})).toBe('{}');
+      expect(HierarchyUtil.ensureUnicodePythonCompatible([])).toBe('[]');
+    });
+
+    test('returns null when error is thrown', () => {
+      const replaceSpy = jest.spyOn(String.prototype, 'replace');
+      replaceSpy.mockImplementation(() => {
+        throw new Error('Invalid value');
+      });
+
+      expect(HierarchyUtil.ensureUnicodePythonCompatible('h"i', true)).toBe(null);
+      replaceSpy.mockRestore();
+    });
+  });
+
+  describe('ensureListUnderLimit', () => {
+    test('returns full list when under limit', () => {
+      expect(HierarchyUtil.ensureListUnderLimit([123, 'abc'], 100)).toEqual([123, 'abc']);
+    });
+
+    test('drops second element when over limit', () => {
+      expect(HierarchyUtil.ensureListUnderLimit([123, 'abc'], 5)).toEqual([123]);
+    });
+
+    test('returns empty list when all elements exceed limit', () => {
+      expect(HierarchyUtil.ensureListUnderLimit([123, 'abc'], 2)).toEqual([]);
+    });
+
+    test('handles null values correctly', () => {
+      const arr = [null, null, 12, null, 'hello'];
+      expect(HierarchyUtil.ensureListUnderLimit(arr, 15)).toEqual([null, null, 12, null]);
+    });
+
+    test('handles Unicode characters correctly', () => {
+      expect(HierarchyUtil.ensureListUnderLimit(['😊😊 .', '.', '💛', '😊'], 6)).toEqual(['😊😊 .', '.', '💛']);
+    });
+
+    test('handles surrogate pairs correctly', () => {
+      expect(HierarchyUtil.ensureListUnderLimit(['\uD83D', '\uDE0A', '\uD83D', '\uDE0A', '.'], 3)).toEqual([
+        '\uD83D',
+        '\uDE0A',
+        '\uD83D',
+      ]);
+    });
+
+    test('returns length 4 when error is thrown in ensureUnicodePythonCompatible', () => {
+      const replaceSpy = jest.spyOn(Object, 'entries');
+      // eslint-disable-next-line
+      // @ts-ignore
+      replaceSpy.mockReturnValue(null);
+
+      expect(HierarchyUtil.ensureListUnderLimit([{}], 3)).toEqual([]);
+      expect(HierarchyUtil.ensureListUnderLimit([{}], 4)).toEqual([{}]);
+    });
   });
 });
