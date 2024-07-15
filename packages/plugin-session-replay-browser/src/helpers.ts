c5efd30e3e6e82696ddec8cc70db85b13b8cc4c5
Kelly Wallach <kelly.wallach@amplitude.com>
2024-07-15 11:48:38 -0400

    fix(session replay plugin): remove unused if check

--- packages/plugin-session-replay-browser/src/helpers.ts
+++ packages/plugin-session-replay-browser/src/helpers.ts
@@ -9,10 +9,7 @@ export const parseUserProperties = (event: Event) => {
   const userPropertyKeys = Object.keys(event.user_properties);
 
   userPropertyKeys.forEach((identifyKey) => {
-    if (
-      Object.values(IdentifyOperation).includes(identifyKey as IdentifyOperation) &&
-      PROPERTY_ADD_OPERATIONS.includes(identifyKey as IdentifyOperation)
-    ) {
+    if (PROPERTY_ADD_OPERATIONS.includes(identifyKey as IdentifyOperation)) {
       const typedUserPropertiesOperation =
         event.user_properties && (event.user_properties[identifyKey as IdentifyOperation] as Record<string, any>);
       userPropertiesObj = {
