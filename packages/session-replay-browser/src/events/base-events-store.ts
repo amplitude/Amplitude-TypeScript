91e729b7322bfa1bc421628995e4144e1b48b554
Lew Gordon <lew.gordon@amplitude.com>
2024-10-07 15:06:21 -0400

    fix(session-replay-browser): unifying how the memory splitting works

---
+++ packages/session-replay-browser/src/events/base-events-store.ts
@@ -0,0 +1,55 @@
+import { MAX_EVENT_LIST_SIZE_IN_BYTES, MAX_INTERVAL, MIN_INTERVAL } from '../constants';
+import { Events, EventsStore, SendingSequencesReturn } from '../typings/session-replay';
+import { Logger } from '@amplitude/analytics-types';
+
+export type InstanceArgs = {
+  loggerProvider: Logger;
+  minInterval?: number;
+  maxInterval?: number;
+  maxPersistedEventsSize?: number;
+};
+
+export abstract class BaseEventsStore<KeyType> implements EventsStore<KeyType> {
+  protected readonly loggerProvider: Logger;
+  private minInterval = MIN_INTERVAL;
+  private maxInterval = MAX_INTERVAL;
+  private maxPersistedEventsSize = MAX_EVENT_LIST_SIZE_IN_BYTES;
+  private interval = this.minInterval;
+  private timeAtLastSplit = Date.now(); // Initialize this so we have a point of comparison when events are recorded
+
+  constructor(args: InstanceArgs) {
+    this.loggerProvider = args.loggerProvider;
+    this.minInterval = args.minInterval ?? this.minInterval;
+    this.maxInterval = args.maxInterval ?? this.maxInterval;
+    this.maxPersistedEventsSize = args.maxPersistedEventsSize ?? this.maxPersistedEventsSize;
+  }
+
+  abstract addEventToCurrentSequence(
+    sessionId: number,
+    event: string,
+  ): Promise<SendingSequencesReturn<KeyType> | undefined>;
+  abstract getSequencesToSend(): Promise<SendingSequencesReturn<KeyType>[] | undefined>;
+  abstract storeCurrentSequence(sessionId: number): Promise<SendingSequencesReturn<KeyType> | undefined>;
+  abstract storeSendingEvents(sessionId: number, events: Events): Promise<KeyType | undefined>;
+  abstract cleanUpSessionEventsStore(sessionId: number, sequenceId: KeyType): Promise<void>;
+
+  /**
+   * Determines whether to send the events list to the backend and start a new
+   * empty events list, based on the size of the list as well as the last time sent
+   * @param nextEventString
+   * @returns boolean
+   */
+  shouldSplitEventsList = (events: Events, nextEventString: string): boolean => {
+    const sizeOfNextEvent = new Blob([nextEventString]).size;
+    const sizeOfEventsList = new Blob(events).size;
+    if (sizeOfEventsList + sizeOfNextEvent >= this.maxPersistedEventsSize) {
+      return true;
+    }
+    if (Date.now() - this.timeAtLastSplit > this.interval && events.length) {
+      this.interval = Math.min(this.maxInterval, this.interval + this.minInterval);
+      this.timeAtLastSplit = Date.now();
+      return true;
+    }
+    return false;
+  };
+}
