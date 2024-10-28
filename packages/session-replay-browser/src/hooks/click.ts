af5bab31b52ba64ce17a626dacde79444c9bcfe3
Jesse Wang <jesse.wang@amplitude.com>
2024-10-28 15:51:11 -0700

    chore(session replay): updating undefined click interactions

--- packages/session-replay-browser/src/hooks/click.ts
+++ packages/session-replay-browser/src/hooks/click.ts
@@ -86,6 +86,9 @@ export const clickHook: (options: Options) => mouseInteractionCallBack =
     }
 
     const { x, y } = e;
+    if (x === undefined || y === undefined) {
+      return;
+    }
 
     const node = record.mirror.getNode(e.id);
     let selector;
@@ -96,8 +99,8 @@ export const clickHook: (options: Options) => mouseInteractionCallBack =
     const { left: scrollX, top: scrollY } = utils.getWindowScroll(globalScope as unknown as Window);
 
     const event: ClickEvent = {
-      x: ( x ?? 0 ) + scrollX,
-      y: ( y ?? 0 ) + scrollY,
+      x: x + scrollX,
+      y: y + scrollY,
       selector,
 
       viewportHeight: innerHeight,
