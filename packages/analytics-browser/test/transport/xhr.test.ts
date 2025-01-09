4664d6f7cdff61884519b42422eb8595770ad42b
izaaz.yunus <izaaz@amplitude.com>
2025-01-09 10:50:24 -0800

    fix: update SR sdk versions and update test app

--- packages/analytics-browser/test/transport/xhr.test.ts
+++ packages/analytics-browser/test/transport/xhr.test.ts
@@ -49,4 +49,52 @@ describe('xhr', () => {
       expect(send).toHaveBeenCalledWith(JSON.stringify(payload));
     });
   });
+
+  describe('send with invalid response', () => {
+    test.each([
+      ['{}'], // ideally response body should be json format to an application/json request
+      [''], // test the edge case where response body is non-json format
+      ['<'],
+    ])('should resolve with response', async (body) => {
+      const transport = new XHRTransport();
+      const url = 'http://localhost:3000';
+      const payload = {
+        api_key: '',
+        events: [],
+      };
+      const result = {
+        statusCode: 200,
+        status: Status.Success as const,
+        body: {
+          eventsIngested: 0,
+          payloadSizeBytes: 0,
+          serverUploadTime: 0,
+        },
+      };
+      const xhr = new XMLHttpRequest();
+      const open = jest.fn();
+      const setRequestHeader = jest.fn();
+      const send = jest.fn();
+      const mock = {
+        ...xhr,
+        open,
+        setRequestHeader,
+        send,
+        readyState: 4,
+        responseText: body,
+      };
+      jest.spyOn(window, 'XMLHttpRequest').mockReturnValueOnce(mock);
+      jest.spyOn(transport, 'buildResponse').mockReturnValueOnce(result);
+
+      const unresolvedResponse = transport.send(url, payload);
+      expect(mock.onreadystatechange).toBeDefined();
+      mock.onreadystatechange && mock.onreadystatechange(new Event(''));
+      const response = await unresolvedResponse;
+      expect(response).toBe(result);
+      expect(open).toHaveBeenCalledWith('POST', url, true);
+      expect(setRequestHeader).toHaveBeenCalledTimes(2);
+      expect(send).toHaveBeenCalledTimes(1);
+      expect(send).toHaveBeenCalledWith(JSON.stringify(payload));
+    });
+  });
 });
--- packages/plugin-session-replay-react-native/example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  PluginSessionReplayReactNativeExample
-//
-
-import Foundation
