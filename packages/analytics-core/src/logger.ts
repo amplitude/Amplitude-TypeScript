f819d03a41cfc5a933a0e9e897b7d38cceac5729
Jesse Wang <jesse.wang@amplitude.com>
2025-02-20 17:36:18 -0800

    Revert "feat(session replay): add rrweb log key to prevent infinite depth error"
    This reverts commit 0de8d6f60c4726f8dc6bb4ee09a644887035f554.

--- packages/analytics-core/src/logger.ts
+++ packages/analytics-core/src/logger.ts
@@ -5,15 +5,8 @@ const PREFIX = 'Amplitude Logger ';
 export class Logger implements ILogger {
   logLevel: LogLevel;
 
-  private readonly defaultLog: typeof console.log;
-  private readonly defaultWarn: typeof console.warn;
-  private readonly defaultError: typeof console.error;
-
   constructor() {
     this.logLevel = LogLevel.None;
-    this.defaultLog = Logger.getDefaultConsoleMethod(console.log);
-    this.defaultWarn = Logger.getDefaultConsoleMethod(console.warn);
-    this.defaultError = Logger.getDefaultConsoleMethod(console.error);
   }
 
   disable(): void {
@@ -28,22 +21,21 @@ export class Logger implements ILogger {
     if (this.logLevel < LogLevel.Verbose) {
       return;
     }
-
-    this.defaultLog(`${PREFIX}[Log]: ${args.join(' ')}`);
+    console.log(`${PREFIX}[Log]: ${args.join(' ')}`);
   }
 
   warn(...args: any[]): void {
     if (this.logLevel < LogLevel.Warn) {
       return;
     }
-    this.defaultWarn(`${PREFIX}[Warn]: ${args.join(' ')}`);
+    console.warn(`${PREFIX}[Warn]: ${args.join(' ')}`);
   }
 
   error(...args: any[]): void {
     if (this.logLevel < LogLevel.Error) {
       return;
     }
-    this.defaultError(`${PREFIX}[Error]: ${args.join(' ')}`);
+    console.error(`${PREFIX}[Error]: ${args.join(' ')}`);
   }
 
   debug(...args: any[]): void {
@@ -51,10 +43,6 @@ export class Logger implements ILogger {
       return;
     }
     // console.debug output is hidden by default in chrome
-    this.defaultLog(`${PREFIX}[Debug]: ${args.join(' ')}`);
-  }
-
-  private static getDefaultConsoleMethod<T extends (...args: any[]) => void>(method: T): T {
-    return (method as { __rrweb_original__?: T })['__rrweb_original__'] || method;
+    console.log(`${PREFIX}[Debug]: ${args.join(' ')}`);
   }
 }
