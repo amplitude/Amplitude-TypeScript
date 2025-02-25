4f2011053d1fc57b1ba39347f3b24964af803cf0
Xinyi Ye <xinyi.ye@amplitude.com>
2025-02-25 15:34:28 -0800

    chore: move autocapture plugin to main

--- packages/plugin-autocapture-browser/rollup.config.js
+++
@@ -1,6 +0,0 @@
-import { iife, umd } from '../../scripts/build/rollup.config';
-
-iife.input = umd.input;
-iife.output.name = 'amplitudeAutocapturePlugin';
-
-export default [umd, iife];
--- packages/plugin-autocapture-browser/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { autocapturePlugin as plugin, autocapturePlugin } from './autocapture-plugin';
-export { Action, ActionData, Message, WindowMessenger } from './libs/messenger';
