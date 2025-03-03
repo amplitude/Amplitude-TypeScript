5b93741aa684aa6d6bd0089c1df5cdff3c1bea8c
Xinyi Ye <xinyi.ye@amplitude.com>
2025-03-03 15:46:33 -0800

    feat(analytics-core): merge analytics-client-common

---
+++ packages/analytics-browser/src/session.ts
@@ -0,0 +1,6 @@
+export const isNewSession = (sessionTimeout: number, lastEventTime: number = Date.now()): boolean => {
+  const currentTime = Date.now();
+  const timeSinceLastEvent = currentTime - lastEventTime;
+
+  return timeSinceLastEvent > sessionTimeout;
+};
