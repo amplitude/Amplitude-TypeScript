4b9a5765d75bb1c8df9240522c0752f8b776f49a
Xinyi Ye <xinyi.ye@amplitude.com>
2025-03-06 15:10:51 -0800

    feat(analytics-core): merge analytics-types
    - event bridge

--- packages/analytics-core/src/event-bridge/event-bridge.ts
+++ packages/analytics-core/src/event-bridge/event-bridge.ts
@@ -1,6 +1,14 @@
-import { EventBridgeReceiver, EventBridge as IEventBridge } from '@amplitude/analytics-types';
 import { Event } from '../event/event';
 import { EventBridgeChannel } from './event-bridge-channel';
+
+export interface IEventBridge {
+  sendEvent(channel: string, event: Event): void;
+  setReceiver(channel: string, receiver: IEventBridgeReceiver): void;
+}
+
+export interface IEventBridgeReceiver {
+  receive(channel: string, event: Event): void;
+}
 
 export class EventBridge implements IEventBridge {
   eventBridgeChannels: Record<string, EventBridgeChannel | undefined> = {};
@@ -12,7 +20,7 @@ export class EventBridge implements IEventBridge {
     (this.eventBridgeChannels[channel] as EventBridgeChannel).sendEvent(event);
   }
 
-  setReceiver(channel: string, receiver: EventBridgeReceiver) {
+  setReceiver(channel: string, receiver: IEventBridgeReceiver) {
     if (!this.eventBridgeChannels[channel]) {
       this.eventBridgeChannels[channel] = new EventBridgeChannel(channel);
     }
