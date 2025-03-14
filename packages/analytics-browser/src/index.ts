7e9eb0cc7ce4e19ef15a557ef9a4c3202ce8cb4d
Xinyi Ye <xinyi.ye@amplitude.com>
2025-03-14 14:25:59 -0700

    fix(analytics-browser): replace analytics-types with analytics-core

--- packages/analytics-browser/src/index.ts
+++ packages/analytics-browser/src/index.ts
@@ -25,4 +25,33 @@ export const {
 } = client;
 export { runQueuedFunctions } from './utils/snippet-helper';
 export { Revenue, Identify } from '@amplitude/analytics-core';
-export * as Types from '@amplitude/analytics-types';
+
+import {
+  DEFAULT_ACTION_CLICK_ALLOWLIST,
+  DEFAULT_CSS_SELECTOR_ALLOWLIST,
+  DEFAULT_DATA_ATTRIBUTE_PREFIX,
+  IdentifyOperation,
+  LogLevel,
+  OfflineDisabled,
+  RevenueProperty,
+  ServerZone,
+  SpecialEventType,
+  Status,
+} from '@amplitude/analytics-core';
+
+export const Types = {
+  DEFAULT_ACTION_CLICK_ALLOWLIST,
+  DEFAULT_CSS_SELECTOR_ALLOWLIST,
+  DEFAULT_DATA_ATTRIBUTE_PREFIX,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  IdentifyOperation,
+  LogLevel,
+  OfflineDisabled,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  RevenueProperty,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  ServerZone,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  SpecialEventType,
+  Status,
+};
