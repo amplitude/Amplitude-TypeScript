6704469add10abbf01d8257445736484311b26c5
Daniel Graham <daniel.graham@amplitude.com>
2025-04-09 17:19:11 -0700

    again

--- packages/analytics-core/test/utils/uuid.test.ts
+++ packages/analytics-core/test/utils/uuid.test.ts
@@ -1,13 +1,13 @@
 import { UUID } from '../../src/utils/uuid';
 
 describe('UUID', () => {
-  test('should generate a valid UUID-4 (web)', () => {
+  test('should generate a valid UUID-4', () => {
     const uuid = UUID();
     expect(uuid.length).toEqual(36);
     expect(uuid.substring(14, 15)).toEqual('4');
   });
   
-  test('should generate a valid UUID-4 (nodejs)', () => {
+  test('should generate a valid UUID-4 (no native Crypto)', () => {
     const backupCrypto = global.crypto;
     Object.defineProperties(global, {
       crypto: {
