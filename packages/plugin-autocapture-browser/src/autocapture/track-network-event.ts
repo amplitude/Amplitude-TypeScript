3107d5763ddf1c6764cbe19bdbabb0bd84bc6791
Daniel Graham <daniel.graham@amplitude.com>
2025-04-21 11:38:59 -0700

    again

--- packages/plugin-autocapture-browser/src/autocapture/track-network-event.ts
+++
@@ -1,9 +0,0 @@
-//import { AutoCaptureOptionsWithDefaults } from "src/autocapture-plugin";
-
-export function shouldTrackNetworkEvent(/*options: AutoCaptureOptionsWithDefaults*/) {
-  throw new Error('Not implemented');
-}
-
-export function trackNetworkEvent() {
-  throw new Error('Not implemented');
-}
