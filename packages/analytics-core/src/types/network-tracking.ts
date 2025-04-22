682e7dbf3a585c3dd48b06da94430028ab652cad
Daniel Graham <daniel.graham@amplitude.com>
2025-04-21 17:54:58 -0700

    chore: add "networkTracking" to autocapture config (#1036)

---
+++ packages/analytics-core/src/types/network-tracking.ts
@@ -0,0 +1,36 @@
+export interface NetworkTrackingOptions {
+  /**
+   * Suppresses tracking Amplitude requests from network capture.
+   * @defaultValue `true`
+   */
+  ignoreAmplitudeRequests?: boolean;
+  /**
+   * Hosts to ignore for network capture. Supports wildcard.
+   * @defaultValue `[]`
+   */
+  ignoreHosts?: string[];
+  /**
+   * Rules to determine which network requests should be captured.
+   *
+   * Performs matching on array in reverse order.
+   */
+  captureRules?: NetworkCaptureRule[];
+}
+
+export interface NetworkCaptureRule {
+  /**
+   * Hosts to allow for network capture. Supports wildcard.
+   * @defaultValue `["*"]` all hosts (except amplitude)
+   */
+  hosts?: string[];
+  /**
+   * Range list that defines the status codes to be captured.
+   * @defaultValue `0,500-599`
+   */
+  statusCodeRange?: string[];
+  /**
+   * Threshold for what is classified as a slow request (in seconds).
+   * @defaultValue `3`
+   */
+  // slowThreshold?: number;
+}
