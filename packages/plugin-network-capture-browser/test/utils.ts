1ec17e66315749c1283020a57a5b7684765df419
Daniel Graham <daniel.graham@amplitude.com>
2025-04-29 14:37:54 -0700

    again

--- packages/plugin-network-capture-browser/test/utils.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-restricted-globals */
-export const mockWindowLocationFromURL = (url: URL) => {
-  window.location.href = url.toString();
-  window.location.search = url.search;
-  window.location.hostname = url.hostname;
-  window.location.pathname = url.pathname;
-};
