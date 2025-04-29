34aec4394ed7ffc4d20c014be888fa4f7eed9c79
Daniel Graham <daniel.graham@amplitude.com>
2025-04-29 12:28:30 -0700

    refactor: move network-capture to a new package

---
+++ packages/plugin-network-capture-browser/src/index.ts
@@ -0,0 +1,2 @@
+export { networkCapturePlugin as plugin, networkCapturePlugin as autocapturePlugin } from './network-capture-plugin';
+export { Action, ActionData, Message, WindowMessenger } from './libs/messenger';
