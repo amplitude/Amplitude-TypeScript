0bdee79b2f7e32eab2b780d507d85f62a32d16c0
Daniel Graham <daniel.graham@amplitude.com>
2025-04-30 13:49:03 -0700

    chore: add a dev server to run SDK on

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import path from 'path';
+import fs from 'fs';
+
+const packagesDir = path.resolve(__dirname, 'packages');
+const testServerDir = path.resolve(__dirname, 'test-server');
+
+const ignorePkg = (pkgName) => {
+  return pkgName.startsWith('.') ||
+    pkgName === 'analytics-browser-test' ||
+    pkgName === 'analytics-node-test';
+}
+
+const amplitudeAliases = fs.readdirSync(packagesDir).reduce((aliases, pkgName) => {
+  const fullPath = path.join(packagesDir, pkgName);
+  if (ignorePkg(pkgName)) {
+    return aliases;
+  }
+  const key = `@amplitude/${pkgName}`;
+  aliases[key] = fullPath;
+  return aliases;
+}, {});
+
+export default defineConfig({
+  root: testServerDir,
+  resolve: {
+    alias: amplitudeAliases
+  },
+  server: {
+    fs: {
+      allow: [
+        packagesDir,
+        testServerDir,
+      ]
+    },
+    watch: {
+      usePolling: true,
+      interval: 100,
+      ignored: ['**/node_modules/**', '**/dist/**'],
+    },
+  },
+  optimizeDeps: {
+  },
+});
