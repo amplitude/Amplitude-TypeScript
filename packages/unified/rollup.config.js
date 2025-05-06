2000dda326367ab65f55ef3232794d846cb077c9
Jesse Wang <jesse.wang@amplitude.com>
2025-05-06 15:26:48 -0700

    chore(session replay): add todo for dynamic import in unified

--- packages/unified/rollup.config.js
+++ packages/unified/rollup.config.js
@@ -1,5 +1,6 @@
 import { umd } from '../../scripts/build/rollup.config';
 
+// Disable dynamic imports to avoid issues with the session replay browser
 export default [{
     ...umd,
     inlineDynamicImports: true,
