20039869201764caeb5d13e395d6f9ebb1f7749a
Joe Pollock <joe.pollock@amplitude.com>
2025-05-09 14:08:48 -0700

    feat: configure ESM build for segment-session-replay-wrapper

--- packages/segment-session-replay-wrapper/rollup.config.js
+++ packages/segment-session-replay-wrapper/rollup.config.js
@@ -1,7 +1,47 @@
-import { umd } from '../../scripts/build/rollup.config';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import gzip from 'rollup-plugin-gzip';
+import { terser } from 'rollup-plugin-terser';
 
-// Disable dynamic imports to avoid issues with the session replay browser
-export default [{
-    ...umd,
-    inlineDynamicImports: true,
-}];
+// Configure ESM build
+const esm = {
+  input: 'src/index.ts',
+  output: {
+    dir: 'lib/scripts',
+    format: 'es',
+    sourcemap: true,
+    entryFileNames: 'segment-session-replay-wrapper-esm.js',
+    chunkFileNames: '[name]-min.js',
+  },
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.json',
+      compilerOptions: {
+        target: 'es2015',
+        module: 'es2020',
+        moduleResolution: 'node',
+        downlevelIteration: true,
+        declaration: false,
+        declarationMap: false,
+        outDir: 'lib/scripts',
+        baseUrl: '.',
+        paths: {
+          'src/*': ['src/*'],
+        },
+      },
+    }),
+    resolve({
+      browser: true,
+    }),
+    commonjs(),
+    terser({
+      output: {
+        comments: false,
+      },
+    }),
+    gzip(),
+  ],
+};
+
+export default [esm];
