3b1270c99223eea6f80e3dddaa91905bee5b0890
jpollock-ampl <joe.pollock@amplitude.com>
2025-05-13 10:24:08 -0700

    refactor: rename package from segment-session-replay-wrapper to segment-session-replay-plugin (#1082)

--- packages/segment-session-replay-plugin/src/constants.ts
+++ packages/segment-session-replay-plugin/src/constants.ts
@@ -1,5 +1,5 @@
 export const COOKIE_NAME = 'amp_session_id';
-export const DEBUG_LOG_PREFIX = 'Amplitude Segment Wrapper [Debug]:';
+export const DEBUG_LOG_PREFIX = 'Amplitude Session Replay Segment Plugin [Debug]:';
 export const INITIAL_EVENT_NAME = 'Immediate Event';
 export const PLUGIN_NAME = 'Session Replay Events';
 export const PLUGIN_TYPE = 'enrichment'; // https://github.com/segmentio/analytics-next/blob/c45d445/packages/core/src/plugins/index.ts#L11
