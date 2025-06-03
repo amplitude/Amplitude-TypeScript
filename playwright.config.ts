8bfdaba7377a002569fa15e37f20abb56c3a9d66
Daniel Graham <daniel.graham@amplitude.com>
2025-06-03 08:29:52 -0700

    chore: run tests via Lerna

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './packages/analytics-browser-test/e2e',
+  testDir: '.',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
