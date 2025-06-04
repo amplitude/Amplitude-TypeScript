4cf2ddc8a47546fd466169a95c74ead26cefa926
Joseph Jin <joseph.jin@amplitude.com>
2025-06-04 14:59:17 -0700

    chore: adding sr targeting

--- packages/session-replay-browser/src/config/joined-config.ts
+++ packages/session-replay-browser/src/config/joined-config.ts
@@ -60,6 +60,7 @@ export class SessionReplayJoinedConfigGenerator {
       if (namespaceConfig) {
         const samplingConfig = namespaceConfig.sr_sampling_config;
         const privacyConfig = namespaceConfig.sr_privacy_config;
+        const targetingConfig = namespaceConfig.sr_targeting_config;
 
         const ugcFilterRules = config.interactionConfig?.ugcFilterRules;
         // This is intentionally forced to only be set through the remote config.
@@ -80,6 +81,12 @@ export class SessionReplayJoinedConfigGenerator {
             remoteConfig.sr_privacy_config = privacyConfig;
           }
         }
+        if (targetingConfig) {
+          if (!remoteConfig) {
+            remoteConfig = {};
+          }
+          remoteConfig.sr_targeting_config = targetingConfig;
+        }
       }
     } catch (err: unknown) {
       const knownError = err as Error;
@@ -95,7 +102,11 @@ export class SessionReplayJoinedConfigGenerator {
       };
     }
 
-    const { sr_sampling_config: samplingConfig, sr_privacy_config: remotePrivacyConfig } = remoteConfig;
+    const {
+      sr_sampling_config: samplingConfig,
+      sr_privacy_config: remotePrivacyConfig,
+      sr_targeting_config: targetingConfig,
+    } = remoteConfig;
     if (samplingConfig && Object.keys(samplingConfig).length > 0) {
       if (Object.prototype.hasOwnProperty.call(samplingConfig, 'capture_enabled')) {
         config.captureEnabled = samplingConfig.capture_enabled;
@@ -177,6 +188,10 @@ export class SessionReplayJoinedConfigGenerator {
         joinedPrivacyConfig,
         this.localConfig.loggerProvider,
       );
+    }
+
+    if (targetingConfig && Object.keys(targetingConfig).length > 0) {
+      config.targetingConfig = targetingConfig;
     }
 
     this.localConfig.loggerProvider.debug(
