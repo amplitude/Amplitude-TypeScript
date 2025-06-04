0822c7d75dc96a2a2110b1946fe793f67f6bb691
Joseph Jin <joseph.jin@amplitude.com>
2025-06-04 15:17:28 -0700

    fix: not returning values

--- packages/session-replay-browser/src/session-replay.ts
+++ packages/session-replay-browser/src/session-replay.ts
@@ -190,7 +190,7 @@ export class SessionReplay implements AmplitudeSessionReplay {
 
     this.teardownEventListeners(false);
 
-    await this.evaluateTargetingAndCapture({ userProperties: options.userProperties });
+    await this.evaluateTargetingAndCapture({ userProperties: options.userProperties }, true);
   }
 
   setSessionId(sessionId: string | number, deviceId?: string) {
@@ -383,12 +383,11 @@ export class SessionReplay implements AmplitudeSessionReplay {
         this.loggerProvider.log(
           `Not capturing replays for session ${this.identifiers.sessionId} due to not matching targeting conditions.`,
         );
-        return true;
+        return false;
       }
       this.loggerProvider.log(
         `Capturing replays for session ${this.identifiers.sessionId} due to matching targeting conditions.`,
       );
-      return false;
     } else {
       const isInSample = isSessionInSample(this.identifiers.sessionId, this.config.sampleRate);
       if (!isInSample) {
@@ -396,6 +395,8 @@ export class SessionReplay implements AmplitudeSessionReplay {
       }
       return isInSample;
     }
+
+    return true;
   }
 
   getBlockSelectors(): string | string[] | undefined {
