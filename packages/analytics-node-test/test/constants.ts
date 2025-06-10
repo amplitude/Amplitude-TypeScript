94919c515efb282dc33c1e48055827a0c45b54a9
Xinyi Ye <xinyi.ye@amplitude.com>
2025-06-10 15:48:07 -0700

    chore: delete node test package

--- packages/analytics-node-test/test/constants.ts
+++
@@ -1,4 +0,0 @@
-export const uuidPattern = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
-export const url = 'https://api2.amplitude.com';
-export const path = '/2/httpapi';
-export const SUCCESS_MESSAGE = 'Event tracked successfully';
