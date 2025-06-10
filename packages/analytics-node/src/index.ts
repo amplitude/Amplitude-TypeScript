02a7039f9b6b30bee308a3e3a0abcbd936004a6e
Xinyi Ye <xinyi.ye@amplitude.com>
2025-06-10 15:42:17 -0700

    chore: delete client common and node packages

--- packages/analytics-node/src/index.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/unbound-method */
-
-import client from './node-client';
-export { createInstance } from './node-client';
-export const { add, groupIdentify, identify, init, logEvent, remove, revenue, setGroup, setOptOut, track, flush } =
-  client;
-export { Revenue, Identify } from '@amplitude/analytics-core';
-export * as Types from '@amplitude/analytics-types';
