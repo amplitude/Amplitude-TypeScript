3a400114a7394d8fd7ee113711e0f4958c39588f
Jesse Wang <jesse.wang@amplitude.com>
2025-06-30 17:46:41 -0700

    chore(session-replay): fix jest setup for encoder

--- packages/session-replay-browser/test/jest-setup.js
+++ packages/session-replay-browser/test/jest-setup.js
@@ -1,8 +1,11 @@
 require('fake-indexeddb/auto');
 const { IDBFactory } = require('fake-indexeddb');
 const structuredClone = require('@ungap/structured-clone');
+const { TextEncoder, TextDecoder } = require('util');
 
 global.structuredClone = structuredClone.default;
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 global.beforeEach(() => {
   global.indexedDB = new IDBFactory();
 });
