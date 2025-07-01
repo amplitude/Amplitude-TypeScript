fade7ded53458cc182ece4ca2243f12f65bbd0c9
Jesse Wang <jesse.wang@amplitude.com>
2025-07-01 13:54:43 -0700

    chore(session-replay): clean-up test files

--- packages/session-replay-browser/test/jest-setup.js
+++ packages/session-replay-browser/test/jest-setup.js
@@ -1,11 +1,8 @@
 require('fake-indexeddb/auto');
 const { IDBFactory } = require('fake-indexeddb');
 const structuredClone = require('@ungap/structured-clone');
-const { TextEncoder, TextDecoder } = require('util');
 
 global.structuredClone = structuredClone.default;
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
 global.beforeEach(() => {
   global.indexedDB = new IDBFactory();
 });
