c4d74910a0cfa3fabab2d2835f295a32b2213f83
Aliaksandr Kazarez <aliaksandr.kazarez@amplitude.com>
2025-07-02 17:10:00 -0700

    feat(segment-session-replay-plugin-react-native): add segment session replay plugin for react native

---
+++ packages/segment-session-replay-plugin-react-native/src/segment-session-replay-plugin.ts
@@ -0,0 +1,92 @@
+import { Plugin, PluginType, type SegmentEvent, EventType, SegmentClient } from '@segment/analytics-react-native';
+
+import {
+  type SessionReplayConfig,
+  getSessionReplayProperties,
+  init,
+  setDeviceId,
+  setSessionId,
+  start,
+  stop,
+} from '@amplitude/session-replay-react-native';
+import { VERSION } from './version';
+
+function getSessionId(event: SegmentEvent): number {
+  const amplitudeSessionId = (event.integrations?.['Actions Amplitude'] as { session_id: number })?.['session_id'];
+  if (amplitudeSessionId) {
+    return amplitudeSessionId ?? -1;
+  }
+
+  if (event.type === EventType.TrackEvent || event.type === EventType.ScreenEvent) {
+    const sessionIdRaw = event.properties?.['session_id'];
+    const sessionId = Number(sessionIdRaw);
+    return Number.isNaN(sessionId) ? -1 : sessionId;
+  }
+  return -1;
+}
+
+function getDeviceId(event: SegmentEvent): string | null {
+  return event.context?.device?.id ?? event.anonymousId ?? null;
+}
+
+export class SegmentSessionReplayPlugin extends Plugin {
+  name = 'amplitude-segment-session-replay-plugin-react-native';
+  version: string = VERSION;
+  type: PluginType = PluginType.enrichment;
+
+  private sessionReplayConfig: SessionReplayConfig;
+
+  // @review: This is to ensure the plugin is initialized before the first event is processed.
+  // because `configure` is not asyncronous
+  private initPromise: Promise<void> | null = null;
+
+  constructor(config: SessionReplayConfig) {
+    super();
+    this.sessionReplayConfig = config;
+  }
+
+  async configure(analytics: SegmentClient): Promise<void> {
+    super.configure(analytics);
+    this.initPromise = init({
+      deviceId: analytics.userInfo.get().anonymousId,
+      ...this.sessionReplayConfig,
+    });
+    await this.initPromise;
+  }
+
+  async execute(event: SegmentEvent): Promise<SegmentEvent> {
+    await this.initPromise;
+
+    const sessionId = getSessionId(event);
+    const deviceId = getDeviceId(event);
+
+    await setSessionId(sessionId);
+    await setDeviceId(deviceId);
+
+    if (event.type === EventType.TrackEvent || event.type === EventType.ScreenEvent) {
+      const properties = await getSessionReplayProperties();
+      event.properties = { ...event.properties, ...properties };
+    }
+
+    return event;
+  }
+
+  async shutdown(): Promise<void> {
+    await this.initPromise;
+    await stop();
+  }
+
+  async start(): Promise<void> {
+    await this.initPromise;
+    await start();
+  }
+
+  async stop(): Promise<void> {
+    await this.initPromise;
+    await stop();
+  }
+}
+
+export function createSegmentSessionReplayPlugin(config: SessionReplayConfig): Plugin {
+  return new SegmentSessionReplayPlugin(config);
+}
