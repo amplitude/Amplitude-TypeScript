6894b93b713c6a2dbdb453155e6261ff7e8ddeb4
Daniel Graham <daniel.graham@amplitude.com>
2025-07-03 15:58:44 -0700

    fix(plugin-autocapture-browser): remove cursor pointer (#1187)

--- packages/plugin-autocapture-browser/src/frustration-plugin.ts
+++ packages/plugin-autocapture-browser/src/frustration-plugin.ts
@@ -87,8 +87,8 @@ export const frustrationPlugin = (options: FrustrationInteractionsOptions): Brow
     }
 
     // Create should track event functions for the different allowlists
-    const shouldTrackRageClick = createShouldTrackEvent(options, rageCssSelectors, true);
-    const shouldTrackDeadClick = createShouldTrackEvent(options, deadCssSelectors, true);
+    const shouldTrackRageClick = createShouldTrackEvent(options, rageCssSelectors);
+    const shouldTrackDeadClick = createShouldTrackEvent(options, deadCssSelectors);
 
     // Create observables for events on the window
     const allObservables = createObservables();
