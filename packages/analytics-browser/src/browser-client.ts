9ad0941c0b87abcb36f903696255497c06eaa48e
Xinyi Ye <xinyi.ye@amplitude.com>
2025-07-03 14:26:17 -0700

    fix(analytics-browser): rename

--- packages/analytics-browser/src/browser-client.ts
+++ packages/analytics-browser/src/browser-client.ts
@@ -49,7 +49,7 @@ import { fileDownloadTracking } from './plugins/file-download-tracking';
 import { DEFAULT_SESSION_END_EVENT, DEFAULT_SESSION_START_EVENT } from './constants';
 import { detNotify } from './det-notification';
 import { networkConnectivityCheckerPlugin } from './plugins/network-connectivity-checker';
-import { applyRemoteConfig } from './config/joined-config';
+import { updateBrowserConfigWithRemoteConfig } from './config/joined-config';
 import { autocapturePlugin } from '@amplitude/plugin-autocapture-browser';
 import { plugin as networkCapturePlugin } from '@amplitude/plugin-network-capture-browser';
 import { WebAttribution } from './attribution/web-attribution';
@@ -98,7 +98,7 @@ export class AmplitudeBrowser extends AmplitudeCore implements BrowserClient {
 
     // Step 2: Create browser config
     // Get default browser config based on browser options
-    let browserOptions = await useBrowserConfig(options.apiKey, options, this);
+    const browserOptions = await useBrowserConfig(options.apiKey, options, this);
 
     // Create remote config client and subscribe to analytics configs
     if (browserOptions.fetchRemoteConfig) {
@@ -108,23 +108,29 @@ export class AmplitudeBrowser extends AmplitudeCore implements BrowserClient {
         browserOptions.serverZone,
       );
 
-      // Subscribe to remote config updates and apply them
-      this.remoteConfig.subscribe(
-        'configs.analyticsSDK.browserSDK',
-        'all',
-        (remoteConfig: RemoteConfig | null, source: Source, lastFetch: Date) => {
-          browserOptions.loggerProvider.debug('Remote configuration received:', {
-            remoteConfig: JSON.stringify(remoteConfig, null, 2),
-            source,
-            lastFetch,
-          });
-          if (remoteConfig) {
-            browserOptions = applyRemoteConfig(remoteConfig, browserOptions);
-            // Update the current instance config
-            this.config = browserOptions;
-          }
-        },
-      );
+      // TODO (check how long will this await take)
+      // 1. what will happen for first time user? Will empty cache return remote config null
+      // 2. what will happen for eixisting user if network is down? Will cache returns immediately to unblock init?
+
+      // Wait for initial remote config before proceeding
+      await new Promise<void>((resolve) => {
+        this.remoteConfig.subscribe(
+          'configs.analyticsSDK.browserSDK',
+          'all',
+          (remoteConfig: RemoteConfig | null, source: Source, lastFetch: Date) => {
+            browserOptions.loggerProvider.debug('Remote configuration received:', {
+              remoteConfig: JSON.stringify(remoteConfig, null, 2),
+              source,
+              lastFetch,
+            });
+            if (remoteConfig) {
+              updateBrowserConfigWithRemoteConfig(remoteConfig, browserOptions);
+            }
+            // Resolve the promise on first callback (initial config)
+            resolve();
+          },
+        );
+      });
     }
 
     await super._init(browserOptions);
