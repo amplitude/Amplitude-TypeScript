8c8d4422a6cd029bd4c66eb3ca4afb8ccc0ec9fc
Tim Yiu <137842098+tyiuhc@users.noreply.github.com>
2025-09-02 13:55:35 -0700

    fix: use original marketing cookies set by web experiment pre-redirect

--- packages/analytics-client-common/src/attribution/web-attribution.ts
+++ packages/analytics-client-common/src/attribution/web-attribution.ts
@@ -10,6 +10,7 @@ export class WebAttribution {
   options: Options;
   storage: Storage<Campaign>;
   storageKey: string;
+  webExpStorageKey: string;
   previousCampaign?: Campaign;
   currentCampaign: Campaign;
   shouldTrackNewCampaign = false;
@@ -26,6 +27,7 @@ export class WebAttribution {
     };
     this.storage = config.cookieStorage as unknown as Storage<Campaign>;
     this.storageKey = getStorageKey(config.apiKey, 'MKTG');
+    this.webExpStorageKey = getStorageKey(config.apiKey, 'MKTG_ORIGINAL');
     this.currentCampaign = BASE_CAMPAIGN;
     this.sessionTimeout = config.sessionTimeout;
     this.lastEventTime = config.lastEventTime;
@@ -44,7 +46,11 @@ export class WebAttribution {
   }
 
   async fetchCampaign() {
-    return await Promise.all([new CampaignParser().parse(), this.storage.get(this.storageKey)]);
+    const originalCampaign = await this.storage.get(this.webExpStorageKey);
+    if (originalCampaign) {
+      await this.storage.remove(this.webExpStorageKey);
+    }
+    return await Promise.all([originalCampaign || new CampaignParser().parse(), this.storage.get(this.storageKey)]);
   }
 
   /**
