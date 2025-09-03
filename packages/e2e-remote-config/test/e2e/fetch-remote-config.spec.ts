ad7d7625d51bda4e73a392b9ff1df28c6bedc0b1
Daniel Graham <daniel.graham@amplitude.com>
2025-09-03 14:34:39 -0700

    fix: properly intercept the URL

--- packages/e2e-remote-config/test/e2e/fetch-remote-config.spec.ts
+++ packages/e2e-remote-config/test/e2e/fetch-remote-config.spec.ts
@@ -2,20 +2,30 @@ import { test, expect } from '@playwright/test';
 
 /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, no-restricted-globals */
 
-// TODO: make this settable via env variable
-const remoteConfigUrl = 'https://sr-client-cfg.amplitude.com/config*';
-
+const remoteConfigUrl = 'https://sr-client-cfg.amplitude.com/';
 const amplitudeVersion = process.env.AMPLITUDE_VERSION;
 const apiKey = process.env.API_KEY;
+let redirectedServerUrl = process.env.REMOTE_CONFIG_URL;
+
+if (!redirectedServerUrl?.endsWith('/')) {
+  redirectedServerUrl += '/';
+}
 
 test.describe(`Fetch remote config sdkVersion=${amplitudeVersion || 'latest'} apiKey=${apiKey || 'default'}`, () => {
   test('should fetch remote config', async ({ page }) => {
     // intercept the fetch request to the remote config
-    await page.route(remoteConfigUrl, async (route) => {
-      await route.continue();
+    await page.route(`${remoteConfigUrl}*`, async (route) => {
+      if (redirectedServerUrl) {
+        // edit the request to redirect to the redirectedServerUrl
+        const request = route.request();
+        const newUrl = request.url().replace(remoteConfigUrl, redirectedServerUrl);
+        await route.continue({ url: newUrl });
+      } else {
+        await route.continue();
+      }
     });
     
-    const fetchRemoteConfigPromise = page.waitForResponse(remoteConfigUrl);
+    const fetchRemoteConfigPromise = page.waitForResponse(`${remoteConfigUrl}*`);
     let queryParams: Record<string, string> = {};
     if (amplitudeVersion) {
       queryParams.amplitudeVersion = amplitudeVersion;
