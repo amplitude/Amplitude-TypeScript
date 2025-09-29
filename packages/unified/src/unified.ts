5c81ad6c99d8a37c520c9fe69b968bbc92e22fca
thomas.kainrad <thomas.kainrad@amplitude.com>
2025-09-29 10:40:49 +0200

    FOO-2348 [wormhole] set enabled to false by default for engagement plugin

--- packages/unified/src/unified.ts
+++ packages/unified/src/unified.ts
@@ -83,7 +83,8 @@ export class AmplitudeUnified extends AmplitudeBrowser implements UnifiedClient 
       this.sessionReplay = (srPlugin as SessionReplayPlugin).sessionReplay;
     }
 
-    await super.add(EngagementPlugin({ ...unifiedOptions?.engagement, ...sharedOptions })).promise;
+    await super.add(EngagementPlugin({ ...{ enabled: false }, ...unifiedOptions?.engagement, ...sharedOptions }))
+      .promise;
   }
 
   /**
