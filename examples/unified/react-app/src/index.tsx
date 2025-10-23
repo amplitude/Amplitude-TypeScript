a47b8c9fb0f59567ef7afb00f70aede885d949d1
Xinyi Ye <xinyi.ye@amplitude.com>
2025-10-23 08:39:22 -0700

    fix(unified): export Identify and Revenue (#1363)

---
+++ examples/unified/react-app/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as amplitude from '@amplitude/unified';
+
+amplitude.initAll('5b9a9510e261f9ead90865bbc5a7ad1d');
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
