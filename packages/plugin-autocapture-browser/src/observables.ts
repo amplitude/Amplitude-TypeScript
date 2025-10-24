6c9c1ebc12b06936db953970f190854d62442cfb
Daniel Graham <daniel.graham@amplitude.com>
2025-10-24 16:44:14 -0700

    again

--- packages/plugin-autocapture-browser/src/observables.ts
+++ packages/plugin-autocapture-browser/src/observables.ts
@@ -60,10 +60,8 @@ export const createClickObservableZen = (
     const handler = (event: MouseEvent | PointerEvent) => {
       observer.next(event);
     };
-    console.log('binding click event');
     getGlobalScope()?.document.addEventListener(clickType, handler);
     return () => {
-      console.log('unbinding click event');
       getGlobalScope()?.document.removeEventListener(clickType, handler);
     };
   });
