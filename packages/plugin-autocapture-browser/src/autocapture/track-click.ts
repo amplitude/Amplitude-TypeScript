c1ebcd4f42921207c7f9dc17f72b0f571c4ddbd5
Daniel Graham <daniel.graham@amplitude.com>
2025-11-04 11:48:52 -0800

    remove comments

--- packages/plugin-autocapture-browser/src/autocapture/track-click.ts
+++ packages/plugin-autocapture-browser/src/autocapture/track-click.ts
@@ -1,6 +1,6 @@
 import { AllWindowObservables, AutoCaptureOptionsWithDefaults } from 'src/autocapture-plugin';
 import { type evaluateTriggersFn } from 'src/helpers';
-import { filter, map } from 'rxjs';
+import { filter, map, delay } from 'rxjs';
 import { BrowserClient } from '@amplitude/analytics-core';
 import { filterOutNonTrackableEvents, shouldTrackEvent } from '../helpers';
 import { AMPLITUDE_ELEMENT_CLICKED_EVENT } from '../constants';
@@ -19,7 +19,9 @@ export function trackClicks({
 }) {
   const { clickObservable } = allObservables;
 
+  // Get buffers of clicks, if the buffer length is over 5, it is rage click
   const bufferedClicks = clickObservable.pipe(
+    delay(0),
     filter(filterOutNonTrackableEvents),
     filter((click) => {
       // Only track clicks on elements that should be tracked,
@@ -29,7 +31,6 @@ export function trackClicks({
   );
 
   return bufferedClicks.subscribe((click) => {
-    /* istanbul ignore next */
     amplitude?.track(AMPLITUDE_ELEMENT_CLICKED_EVENT, click.targetElementProperties);
   });
 }
