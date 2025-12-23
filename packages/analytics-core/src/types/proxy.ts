38405c5fdc1a1893c25592ed6595848c4c978a5f
Daniel Graham <daniel.graham@amplitude.com>
2025-12-22 17:13:04 -0800

    again

---
+++ packages/analytics-core/src/types/proxy.ts
@@ -0,0 +1,14 @@
+import { Result } from './result';
+
+interface ProxyItem {
+  name: string;
+  args: any[];
+  resolve?: (promise: Promise<Result>) => void;
+}
+
+export type QueueProxy = Array<ProxyItem>;
+
+export interface InstanceProxy {
+  _q: QueueProxy;
+  _iq: Record<string, InstanceProxy>;
+}
