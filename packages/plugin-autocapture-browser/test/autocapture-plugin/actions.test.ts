ac986e66fa58173897210823d9d6c3907684b1bf
Daniel Graham <daniel.graham@amplitude.com>
2025-12-24 10:31:11 -0800

    chore: tweak tsconfig.json

--- packages/plugin-autocapture-browser/test/autocapture-plugin/actions.test.ts
+++ packages/plugin-autocapture-browser/test/autocapture-plugin/actions.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+
 import { autocapturePlugin } from '../../src/autocapture-plugin';
 import { type ElementBasedTimestampedEvent } from '../../src/helpers';
 
