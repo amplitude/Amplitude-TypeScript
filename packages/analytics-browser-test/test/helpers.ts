1f5e4d0a69e3e3bbe125f01354664fa26d9dea47
Daniel Graham <daniel.graham@amplitude.com>
2025-12-24 08:16:24 -0800

    again

--- packages/analytics-browser-test/test/helpers.ts
+++ packages/analytics-browser-test/test/helpers.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { BaseEvent, Campaign } from '@amplitude/analytics-core';
-import { BASE_CAMPAIGN } from '@amplitude/analytics-client-common';
+import { BASE_CAMPAIGN } from '@amplitude/analytics-client-core';
 import { uuidPattern } from './constants';
 
 const uuid: string = expect.stringMatching(uuidPattern) as string;
