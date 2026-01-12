067d0ebb213bdd03f135b6c9bde70cedb35e49e9
Daniel Graham <daniel.graham@amplitude.com>
2026-01-12 13:25:13 -0800

    fix(analytics-browser): two cookie problem resolution (#1476)

--- packages/analytics-core/src/storage/browser-storage.ts
+++ packages/analytics-core/src/storage/browser-storage.ts
@@ -1,3 +1,4 @@
+import { UUID } from '../utils/uuid';
 import { Storage as AmplitudeStorage } from '../types/storage';
 
 export class BrowserStorage<T> implements AmplitudeStorage<T> {
@@ -11,7 +12,7 @@ export class BrowserStorage<T> implements AmplitudeStorage<T> {
 
     const random = String(Date.now());
     const testStorage = new BrowserStorage<string>(this.storage);
-    const testKey = 'AMP_TEST';
+    const testKey = `AMP_TEST_${UUID().substring(0, 8)}`;
     try {
       await testStorage.set(testKey, random);
       const value = await testStorage.get(testKey);
