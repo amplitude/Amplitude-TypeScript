04b01fb98c11f8c36882057422168c339bd48ec1
Daniel Graham <daniel.graham@amplitude.com>
2026-02-28 17:20:50 -0800

    again

--- packages/analytics-browser/src/config.ts
+++ packages/analytics-browser/src/config.ts
@@ -302,15 +302,15 @@ export const useBrowserConfig = async (
 
   // Step 1: Create identity storage instance
   const identityStorage = options.identityStorage || DEFAULT_IDENTITY_STORAGE;
-  let topLevelDomain = '';
+  let defaultCookieDomain = '';
 
   // use the getTopLevelDomain function to find the TLD only if identity storage
   // is cookie (because getTopLevelDomain() uses cookies)
   if (identityStorage === DEFAULT_IDENTITY_STORAGE) {
-    topLevelDomain = await getTopLevelDomain(undefined, diagnosticsClient);
+    defaultCookieDomain = await getTopLevelDomain(undefined, diagnosticsClient);
   }
   const cookieOptions = {
-    domain: options.cookieOptions?.domain,
+    domain: options.cookieOptions?.domain ?? defaultCookieDomain,
     expiration: 365,
     sameSite: 'Lax' as const,
     secure: false,
@@ -416,7 +416,7 @@ export const useBrowserConfig = async (
     earlyConfig?.diagnosticsSampleRate ?? amplitudeInstance._diagnosticsSampleRate,
     diagnosticsClient,
     options.remoteConfig,
-    topLevelDomain,
+    defaultCookieDomain,
     options.enableRequestBodyCompression,
     amplitudeInstance._enableRequestBodyCompressionExperimentalValue,
   );
