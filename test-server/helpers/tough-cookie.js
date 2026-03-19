d69f8166931bf1577eab5fbbc20d986cc19ac75c
Daniel Graham <daniel.graham@amplitude.com>
2026-03-18 22:20:10 -0700

    again

--- test-server/helpers/tough-cookie.js
+++ test-server/helpers/tough-cookie.js
@@ -7,24 +7,24 @@ const LEGAL_COOKIE_PATTERNS = [
   /^AMP_[A-Za-z0-9]{10}$/,
 ];
 
-  const desc = Object.getOwnPropertyDescriptor(Document.prototype, "cookie");
+const desc = Object.getOwnPropertyDescriptor(Document.prototype, "cookie");
 
-  Object.defineProperty(document, "cookie", {
-    configurable: true,
-    enumerable: true,
-    get() {
-      return desc.get.call(document);
-    },
-    set(value) {
-      const name = value.split("=")[0].trim();
-      if (!LEGAL_COOKIE_PATTERNS.some((pattern) => {
-        if (pattern instanceof RegExp) {
-          return pattern.test(name);
-        }
-        return pattern === name;
-      })) {
-        throw new Error(`Illegal cookie name: ${name}`)
+Object.defineProperty(document, "cookie", {
+  configurable: true,
+  enumerable: true,
+  get() {
+    return desc.get.call(document);
+  },
+  set(value) {
+    const name = value.split("=")[0].trim();
+    if (!LEGAL_COOKIE_PATTERNS.some((pattern) => {
+      if (pattern instanceof RegExp) {
+        return pattern.test(name);
       }
-      return desc.set.call(document, value);
-    },
-  });
+      return pattern === name;
+    })) {
+      throw new Error(`Illegal cookie name: ${name}`)
+    }
+    return desc.set.call(document, value);
+  },
+});
