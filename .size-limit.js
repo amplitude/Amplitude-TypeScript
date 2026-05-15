4942052a1044103e71af6a41487f078176a4be1f
Daniel Graham <daniel.graham@amplitude.com>
2026-05-15 09:48:14 -0700

    again

--- .size-limit.js
+++ .size-limit.js
@@ -2,16 +2,19 @@ module.exports = [
   {
     // analytics-browser bundle
     path: './packages/analytics-browser/lib/scripts/amplitude-min.js.gz',
-    limit: '60kb',
+    limit: '65kb',
+    brotli: false,
   },
   {
     // session-replay standalone bundle
     path: './packages/session-replay-browser/lib/scripts/session-replay-browser-min.js.gz',
     limit: '150kb',
+    brotli: false,
   },
   {
     // unified SDK bundle
     path: './packages/unified/lib/scripts/amplitude-min.umd.js.gz',
     limit: '225kb',
+    brotli: false,
   },
 ]
