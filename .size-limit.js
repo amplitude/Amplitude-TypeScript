e28dd6be0098f7d2244406199b1a38ce62661360
Daniel Graham <daniel.graham@amplitude.com>
2026-05-20 09:28:38 -0700

    chore: add size limit gating to Amplitude (#1756)

---
+++ .size-limit.js
@@ -0,0 +1,22 @@
+const limits = [
+  {
+    // analytics-browser bundle
+    path: './packages/analytics-browser/lib/scripts/amplitude-min.js.gz',
+    limit: '65kb',
+    brotli: false,
+  },
+  {
+    // session-replay standalone bundle
+    path: './packages/session-replay-browser/lib/scripts/session-replay-browser-min.js.gz',
+    limit: '150kb',
+    brotli: false,
+  },
+  {
+    // unified SDK bundle
+    path: './packages/unified/lib/scripts/amplitude-min.umd.js.gz',
+    limit: '225kb',
+    brotli: false,
+  },
+]
+
+module.exports = limits;
