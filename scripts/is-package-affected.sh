504890b7a7690c256872cecbc9ea2a7078c483fe
Daniel Graham <daniel.graham@amplitude.com>
2026-05-27 11:54:30 -0700

    again

--- scripts/is-package-affected.sh
+++ scripts/is-package-affected.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pnpm exec nx show projects --affected --base=9ef2365e1f4645a675581fe1a4e98e65bb9ead38 --head=HEAD | grep "$1"
+pnpm exec nx show projects --affected --base=main --head=HEAD | grep "$1"
 if [ $? -eq 0 ]; then
   exit 0
 else
