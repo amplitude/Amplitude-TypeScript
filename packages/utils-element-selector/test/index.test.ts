3d111f76967eb7d601c391f1684bbdb95369f820
Jesse Wang <144086244+jxiwang@users.noreply.github.com>
2026-06-02 15:47:27 -0700

    chore: rename utils-element-selector (#1787)

--- packages/utils-element-selector/test/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { PACKAGE_NAME } from '../src';
-
-describe('@amplitude/utils-element-selector — scaffolding smoke test', () => {
-  it('exports the package name', () => {
-    expect(PACKAGE_NAME).toBe('@amplitude/utils-element-selector');
-  });
-});
