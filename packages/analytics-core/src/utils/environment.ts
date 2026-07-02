6fb9b5772440cce1d84e86cc29bee82cfb1f250f
Daniel Graham <daniel.graham@amplitude.com>
2026-07-02 13:59:41 -0700

    refactor(analytics-react-native): support Network Tracking (#1863)

--- packages/analytics-core/src/utils/environment.ts
+++ packages/analytics-core/src/utils/environment.ts
@@ -4,3 +4,8 @@ export function isChromeExtension(): boolean {
   const globalScope = getGlobalScope() as { chrome?: { runtime?: { id?: string } } };
   return typeof globalScope?.chrome?.runtime?.id === 'string';
 }
+
+export function isReactNative(): boolean {
+  const globalScope = getGlobalScope() as { navigator?: { product?: string } };
+  return globalScope?.navigator?.product === 'ReactNative';
+}
