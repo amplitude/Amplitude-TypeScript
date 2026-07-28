c19b447f48f11d74e98f0c313978d49f80037e1d
Daniel Graham <daniel.graham@amplitude.com>
2026-07-28 16:08:19 -0700

    use local variables

--- examples/react-native/expo-app/babel.config.js
+++ examples/react-native/expo-app/babel.config.js
@@ -3,13 +3,13 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      '@amplitude/babel-plugin-autocapture-transformer',
       // Inlines AMPLITUDE_API_KEY from the shell environment at bundle time.
       // `export AMPLITUDE_API_KEY=…` before `pnpm run android` / `pnpm run ios`.
       [
         'transform-inline-environment-variables',
         {include: ['AMPLITUDE_API_KEY', 'TEST_SERVER_HOST']},
       ],
+      '@amplitude/babel-plugin-autocapture-transformer',
     ],
   };
 };
